39bae3a43ef9b4bd51272ea2f171fbb42d522a6d
Francois Romain <francoisromain@gmail.com>
2018-04-05 01:01:36 +0200

    feat: refined topbar style

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="flex">
-    <div class="h2 p-m">Camino</div>
+    <div class="logo pb-m pt pr-m">Camino</div>
     <div class="menu">
       <ul>
-        <li :class="{ active: $route.name === 'titres' }">
+        <li :class="{ active: $route.name === 'titres' || $route.name === 'titre' }">
           <router-link
             :to="{ name: 'titres' }"
-            class="btn-alt px py-m">Titres</router-link>
+            class="btn-menu px pb-m pt">Titres</router-link>
         </li>
       </ul>
     </div>
--- src/components/ui/Page.vue
+++ src/components/ui/Page.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page relative">
-    <div class="topbar bg-bg">
+    <div class="topbar">
       <div class="container">
         <slot name="header" />
       </div>
