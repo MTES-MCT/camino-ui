79eaf20a5da4a985facbe05f1bc7ad319fb2cf6a
Francois Romain <francoisromain@gmail.com>
2018-04-09 18:14:16 +0200

    refactor: separate filters

--- src/components/Titles.vue
+++ src/components/Titles.vue
@@ -19,79 +19,8 @@
       </ul>
     </div>
 
-    <accordion class="mb">
-      <template slot="title">Filtres</template>
-      <div class="tablet-blobs">
-        <div class="tablet-blob-1-2 desktop-blob-1-3">
-          <div class="mb">
-            <h6>Localisation</h6>
-            <input
-              type="text">
-          </div>
-          <div class="mb">
-            <h6>Titulaire</h6>
-            <input
-              type="text">
-          </div>
-          <div class="mb">
-            <h6>Substances</h6>
-            <input
-              type="text">
-          </div>
-        </div>
-        <div class="tablet-blob-1-2 desktop-blob-1-3">
-          <h6>Domaine</h6>
-          <ul class="list-sans">
-            <li
-              v-for="(domaine, domaineCode) in filtres.domaines"
-              :key="domaineCode">
-              <label>
-                <input
-                  type="checkbox"
-                  class="mr-s">
-                <tag
-                  :color="`bg-title-domain-${domaineCode.toLowerCase()}`"
-                  class="mr-xs mono">{{ domaineCode }}</tag>
-                {{ domaine }}
-              </label>
-            </li>
-          </ul>
-        </div>
-        <div class="tablet-blob-1-2 desktop-blob-1-3">
-          <div class="mb">
-            <h6>Type</h6>
-            <ul class="list-sans">
-              <li
-                v-for="(type, typeCode) in filtres.types"
-                :key="typeCode">
-                <label>
-                  <input
-                    type="checkbox"
-                    class="mr-s">
-                  {{ type }}
-                </label>
-              </li>
-            </ul>
-          </div>
-          <div class="mb">
-            <h6>Statuts</h6>
-            <ul class="list-sans">
-              <li
-                v-for="(statut, statutCode) in filtres.statuts"
-                :key="statutCode">
-                <label>
-                  <input
-                    type="checkbox"
-                    class="mr-s">
-                  <title-status :status="statut" />
-                  {{ statut }}
-                </label>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </accordion>
+    <titles-filters />
+
     <component
       :titles="titres"
       :is="viewCurrent" />
@@ -101,11 +30,9 @@
 <script>
 import Card from '@/components/ui/Card.vue'
 import Loader from '@/components/ui/Loader.vue'
-import Tag from '@/components/ui/Tag.vue'
-import Accordion from '@/components/ui/Accordion.vue'
-import TitleStatus from '@/components/camino/TitleStatus.vue'
 import TitlesTable from '@/components/camino/TitlesTable.vue'
 import TitlesMap from '@/components/camino/TitlesMap.vue'
+import TitlesFilters from '@/components/camino/TitlesFilters.vue'
 
 export default {
   name: 'Titres',
@@ -113,9 +40,7 @@ export default {
   components: {
     Loader,
     Card,
-    Tag,
-    Accordion,
-    TitleStatus
+    TitlesFilters
   },
 
   data () {
---
+++ src/components/camino/TitlesFilters.vue
@@ -0,0 +1,100 @@
+<template>
+  <accordion
+    :sub="true"
+    class="mb">
+    <template slot="title">Filtres</template>
+    <template slot="sub">Filtres</template>
+    <div class="tablet-blobs">
+      <div class="tablet-blob-1-2 desktop-blob-1-3">
+        <div class="mb">
+          <h6>Localisation</h6>
+          <input
+            type="text">
+        </div>
+        <div class="mb">
+          <h6>Titulaire</h6>
+          <input
+            type="text">
+        </div>
+        <div class="mb">
+          <h6>Substances</h6>
+          <input
+            type="text">
+        </div>
+      </div>
+      <div class="tablet-blob-1-2 desktop-blob-1-3">
+        <h6>Domaine</h6>
+        <ul class="list-sans">
+          <li
+            v-for="(domaine, domaineCode) in filtres.domaines"
+            :key="domaineCode">
+            <label>
+              <input
+                type="checkbox"
+                class="mr-s">
+              <tag
+                :color="`bg-title-domain-${domaineCode.toLowerCase()}`"
+                class="mr-xs mono">{{ domaineCode }}</tag>
+              {{ domaine }}
+            </label>
+          </li>
+        </ul>
+      </div>
+      <div class="tablet-blob-1-2 desktop-blob-1-3">
+        <div class="mb">
+          <h6>Type</h6>
+          <ul class="list-sans">
+            <li
+              v-for="(type, typeCode) in filtres.types"
+              :key="typeCode">
+              <label>
+                <input
+                  type="checkbox"
+                  class="mr-s">
+                {{ type }}
+              </label>
+            </li>
+          </ul>
+        </div>
+        <div class="mb">
+          <h6>Statuts</h6>
+          <ul class="list-sans">
+            <li
+              v-for="(statut, statutCode) in filtres.statuts"
+              :key="statutCode">
+              <label>
+                <input
+                  type="checkbox"
+                  class="mr-s">
+                <title-status :status="statut" />
+                {{ statut }}
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </accordion>
+</template>
+
+<script>
+import TitleStatus from '@/components/camino/TitleStatus.vue'
+import Tag from '@/components/ui/Tag.vue'
+import Accordion from '@/components/ui/Accordion.vue'
+
+export default {
+  components: {
+    Tag,
+    TitleStatus,
+    Accordion,
+  },
+
+  computed: {
+    filtres () {
+      return this.$store.state.lib.titre.filtres
+    }
+  }
+
+}
+</script>
+
