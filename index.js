1a0ae1e46c528c4758f7ecdfd28f199945b1f1ad
Francois Romain <francoisromain@gmail.com>
2018-05-30 21:36:26 +0200

    fix: compression

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 const path = require('path')
-const history = require('connect-history-api-fallback')
 const express = require('express')
+const history = require('connect-history-api-fallback')
+const compression = require('compression')
+
 const app = express()
 const staticFileMiddleware = express.static(path.join(__dirname, 'dist'))
 const port = process.env.NODE_PORT
 
 app.use(staticFileMiddleware)
-
 app.use(history())
-
 app.use(staticFileMiddleware)
+app.use(compression())
 
 app.get('/', function(req, res) {
   res.render(path.join(__dirname + '/dist/index.html'))
