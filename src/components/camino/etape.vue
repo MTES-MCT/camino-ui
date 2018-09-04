45a93384ddb807d57ad2ef050d18c118119e81f0
Francois Romain <francoisromain@gmail.com>
2018-09-04 14:50:31 +0200

    feat: affichage des propriétés d'une étape

--- src/components/camino/etape.vue
+++ src/components/camino/etape.vue
@@ -5,8 +5,47 @@
   >
     <template slot="title">{{ etape.type.nom }}</template>
     <template slot="sub">
-      <h6>Date</h6>
-      <h4>{{ etape.date | dateFormat }}</h4>
+      <div class="tablet-blobs">
+        <div class="tablet-blob-1-4">
+          <h6>Date</h6>
+          <p>{{ etape.date | dateFormat }}</p>
+        </div>
+        <div 
+          v-if="etape.duree" 
+          class="tablet-blob-1-4"
+        >
+          <h6>Duree</h6>
+          <p>{{ etape.duree }} ans</p>
+        </div>
+        <div 
+          v-if="etape.geojsonMultiPolygon" 
+          class="tablet-blob-1-4"
+        >
+          <h6>Périmètre</h6>
+          <p>–</p>
+        </div>
+        <div 
+          v-if="etape.titulaires.length > 0" 
+          class="tablet-blob-1-4"
+        >
+          <h6>Titulaires</h6>
+          <p>{{ etape.titulaires.map(t => t.nom).join(', ') }}</p>
+        </div>
+        <div 
+          v-if="etape.amodiataires.length > 0" 
+          class="tablet-blob-1-4"
+        >
+          <h6>Amodiataires</h6>
+          <p>{{ etape.amodiataires.map(t => t.nom).join(', ') }}</p>
+        </div>
+        <div 
+          v-if="etape.substances.length > 0" 
+          class="tablet-blob-1-4"
+        >
+          <h6>Substances</h6>
+          <p>{{ etape.substances.map(t => t.nom).join(', ') }}</p>
+        </div>
+      </div>
     </template>
     <documents
       :documents="etape.documents"
