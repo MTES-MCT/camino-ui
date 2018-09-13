10a3c74e6d7616ab046fe05102d95648fa1aab39
Francois Romain <francoisromain@gmail.com>
2018-09-13 10:37:20 +0200

    fix: bug dans safari 11 qui ne supporte pas fetch

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,14 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+// for safari 11
+import fetch from 'unfetch'
+
 import queryTitres from './queries/titres'
 import queryTitre from './queries/titre'
 
 const graphqlClient = new ApolloClient({
-  link: new HttpLink({ uri: process.env.VUE_APP_API_URL }),
+  link: new HttpLink({ uri: process.env.VUE_APP_API_URL, fetch }),
   cache: new InMemoryCache()
 })
 
