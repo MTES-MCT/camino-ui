a2cceced24e510943ca1cb54b719b5055e4df74a
Francois Romain <francoisromain@gmail.com>
2018-10-22 13:29:49 +0200

    feat: ajoute un traceur piwik

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './css/styles.css'
 import Vue from 'vue'
+import VueMatomo from 'vue-matomo'
 import App from './app.vue'
 
 import router from './router'
@@ -13,6 +14,15 @@ Vue.config.productionTip = false
 Vue.filter('dateFormat', dateFormat)
 
 Vue.mixin(mixins)
+
+Vue.use(VueMatomo, {
+  host: 'https://stats.data.gouv.fr',
+  siteId: 70,
+  router,
+  requireConsent: false,
+  trackInitialView: true,
+  trackerFileName: 'piwik'
+})
 
 new Vue({
   router,
