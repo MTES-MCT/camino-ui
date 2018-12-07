8511dae46ab2e9d3ee13e04ef4b5c8f0757e435a
Francois Romain <francoisromain@gmail.com>
2018-12-07 12:19:06 +0100

    refactor: déplace la popup warning dans le dossier user

--- src/app.vue
+++ src/app.vue
@@ -36,7 +36,7 @@ import Messages from './components/ui/messages.vue'
 import PageHeader from './components/page-header.vue'
 import PageFooter from './components/page-footer.vue'
 import Error from './components/error.vue'
-import PopupAvertissement from './components/content/warning-popup.vue'
+import PopupAvertissement from './components/user/warning-popup.vue'
 
 export default {
   name: 'App',
