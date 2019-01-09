e3dcf6b21ea5d6f3e5e67ba9752b629a90c6e47c
Francois Romain <francoisromain@gmail.com>
2019-01-09 10:07:54 +0100

    fix: bug ff 52: supprime la csp

--- index.js
+++ index.js
@@ -15,17 +15,7 @@ const history = require('connect-history-api-fallback')
 const compression = require('compression')
 
 const app = express()
-const staticFileMiddleware = express.static(path.join(__dirname, 'dist'), {
-  setHeaders: (res, path, stat) => {
-    res.set({
-      'Content-Security-Policy':
-        "default-src 'none'; frame-src app.mailjet.com; img-src data: 'self' a.tile.openstreetmap.org b.tile.openstreetmap.org c.tile.openstreetmap.org  a.tile.openstreetmap.fr b.tile.openstreetmap.fr c.tile.openstreetmap.fr geoservices.brgm.fr wxs.ign.fr stats.data.gouv.fr; script-src 'self' stats.data.gouv.fr; style-src 'self'; connect-src api.camino.beta.gouv.fr test.api.camino.beta.gouv.fr api.camino.local",
-      'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
-      'X-XSS-Protection': '1; mode=block'
-    })
-  }
-})
+const staticFileMiddleware = express.static(path.join(__dirname, 'dist'))
 const port = process.env.NODE_PORT
 
 app.use(compression())
