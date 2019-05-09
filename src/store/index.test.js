62a92b2a22e4a69d7a1ebf221ee6ea11c16540c9
Florent Chalvet <florentchalvet@gmail.com>
2019-05-09 19:12:09 +0200

    test: teste user,index

--- src/store/index.test.js
+++ src/store/index.test.js
@@ -1,59 +1,342 @@
-import { actions, mutations, state } from '.index'
+import { actions, mutations } from './index'
 import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
-import * as routerApi from '../router'
+import VueRouter from 'vue-router'
+import * as api from '../api'
 
-jest.mock('../router', () => ({ router: [] }))
+jest.mock('../router', () => ({}))
+
+jest.mock('../api', () => ({ init: jest.fn() }))
+jest.mock('./titre', () => ({ titre: jest.fn() }))
+jest.mock('./titres', () => ({ titres: jest.fn() }))
+jest.mock('./metas', () => ({ metas: jest.fn() }))
+jest.mock('./map', () => ({ map: jest.fn() }))
+jest.mock('./utilisateur', () => ({ utilisateur: jest.fn() }))
+jest.mock('./utilisateurs', () => ({ utilisateurs: jest.fn() }))
+jest.mock('./entreprises', () => ({ entreprises: jest.fn() }))
+jest.mock('./substances', () => ({ substances: jest.fn() }))
+jest.mock('./user', () => ({ user: jest.fn() }))
+jest.mock('./titre-activites', () => ({ titreActivites: jest.fn() }))
 
 const localVue = createLocalVue()
-localVue.use(Vuex)
+localVue.use(Vuex, VueRouter)
 
 console.log = jest.fn()
 
+jest.useFakeTimers()
+
 describe('teste les fonctions utilisées dans les autres scripts', () => {
+  let state
   let store
+  let setMetas, getTitre, getTitres, getUtilisateurs, getUtilisateur
+  let permissionsSetUtilisateurs, setSubstances, setEntreprises
+  let router
   beforeEach(() => {
-    store = new Vuex.Store({ state, actions, mutations })
-  })
-
-  test("renvoie une erreur definie de l'api", async () => {
-    const error = "test de l'erreur OK"
-    await index.dispatch('apiError', error)
-
-    expect(index.state.messages).toEqual([{ type: 'error', value: error }])
-  })
-
-  test("renvoie une erreur de connexion indefiniz de l'api", async () => {
-    await index.dispatch('apiError')
-
-    expect(index.state.messages).toEqual([
-      {
-        type: 'error',
-        value: `Erreur: impossible de se connecter à l'API (undefined)`
-      }
-    ])
-  })
-
-  test('renvoie une erreur de la page', async () => {
-    await index.dispatch('pageError')
-
-    expect(index.state.error).toEqual({
-      type: 'error',
-      value: `Erreur: page introuvable`
-    })
-  })
-})
-
-describe('teste les fonctions utilisant les autres scripts', () => {
-  beforeEach(() => {
-    index.state = {
+    state = {
       config: {},
       messages: [],
       popup: { component: null, props: null, messages: [] },
       error: null,
       menu: { component: null },
+      versions: {
+        api: null,
+        ui: `${npmVersion}`
+      },
       loading: [],
       loaded: false
     }
+    setMetas = jest.fn()
+    getTitre = jest.fn()
+    getTitres = jest.fn()
+    getUtilisateurs = jest.fn()
+    getUtilisateur = jest.fn()
+    permissionsSetUtilisateurs = jest.fn()
+    setSubstances = jest.fn()
+    setEntreprises = jest.fn()
+    router = new VueRouter()
+    store = new Vuex.Store({
+      modules: {
+        titre: { namespaced: true, actions: { get: getTitre } },
+        titres: { namespaced: true, actions: { get: getTitres } },
+        metas: { namespaced: true, actions: { set: setMetas } },
+        utilisateur: { namespaced: true, actions: { get: getUtilisateur } },
+        utilisateurs: {
+          namespaced: true,
+          actions: { get: getUtilisateurs },
+          mutations: { permissionsSet: permissionsSetUtilisateurs }
+        },
+        entreprises: { namespaced: true, mutations: { set: setEntreprises } },
+        substances: { namespaced: true, mutations: { set: setSubstances } }
+      },
+      state,
+      actions,
+      mutations
+    })
+  })
+
+  test('initie tout les différents types de composants', async () => {
+    const apiMock = api.init.mockResolvedValue({
+      version: '10.4',
+      metas: { domaines: 'c,w', statuts: 'val' },
+      substances: 'or',
+      permissions: 'admin',
+      entreprises: 'macdo'
+    })
+    await store.dispatch('init')
+
+    expect(state.versions.api).toEqual('10.4')
+    expect(permissionsSetUtilisateurs).toHaveBeenCalled()
+    expect(setSubstances).toHaveBeenCalled()
+    expect(setEntreprises).toHaveBeenCalled()
+    expect(setMetas).toHaveBeenCalled()
+  })
+
+  test('initie sans composants', async () => {
+    const apiMock = api.init.mockResolvedValue({})
+    await store.dispatch('init')
+
+    expect(state.versions.api).toBeNull()
+    expect(permissionsSetUtilisateurs).not.toHaveBeenCalled()
+    expect(setSubstances).not.toHaveBeenCalled()
+    expect(setEntreprises).not.toHaveBeenCalled()
+    expect(setMetas).not.toHaveBeenCalled()
+  })
+
+  test("erreur dans l'api lors de l'initiation", async () => {
+    const apiMock = api.init.mockRejectedValue(new Error("erreur dans l'api"))
+    await store.dispatch('init')
+
+    expect(state.messages.pop().type).toEqual('error')
+    expect(console.log).toHaveBeenCalled()
+  })
+
+  test("renvoie une erreur definie de l'api", async () => {
+    const error = "erreur dans l'api"
+    await store.dispatch('apiError', error)
+
+    expect(state.messages).toEqual([
+      { type: 'error', value: "erreur dans l'api" }
+    ])
+  })
+
+  test("renvoie une erreur indefinie de l'api", async () => {
+    await store.dispatch('apiError')
+
+    expect(state.messages).toEqual([
+      {
+        type: 'error',
+        value: "Erreur: impossible de se connecter à l'API (undefined)"
+      }
+    ])
+  })
+
+  test('renvoie une erreur de la page', async () => {
+    await store.dispatch('pageError')
+
+    expect(state.error).toEqual({
+      type: 'error',
+      value: `Erreur: page introuvable`
+    })
+  })
+
+  test('ajoute un message et le supprime plus tard', async () => {
+    const message = { id: 14, message: 'message important' }
+    await store.dispatch('messageAdd', message)
+
+    const res = state.messages.pop()
+    expect(res.message).toEqual('message important')
+    expect(res.id).toBeLessThanOrEqual(new Date().valueOf())
+    expect(setTimeout).toHaveBeenCalled()
+    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 4500)
+  })
+
+  test('ferme le menu deja ouvert', async () => {
+    store.state.menu.component = { name: 'bonjour' }
+    const component = { name: 'bonjour' }
+    await store.dispatch('menuToggle', component)
+
+    expect(state.menu.component).toBeNull()
+  })
+
+  test('ferme le menu pour en rouvrir un autre', async () => {
+    store.state.menu.component = { name: 'bonjour' }
+    const component = { name: 'hello' }
+    expect(state.menu.component).toEqual({ name: 'bonjour' })
+    await store.dispatch('menuToggle', component)
+
+    expect(state.menu.component).toEqual(component)
+  })
+
+  test('ouvre un nouveau menu', async () => {
+    const component = { name: 'hello' }
+    await store.dispatch('menuToggle', component)
+
+    expect(state.menu.component).toEqual(component)
+  })
+
+  test('charge le router sur les titres', async () => {
+    router.push({ name: 'titres', params: { id: '123' } })
+    console.log(router.currentRoute)
+    await store.dispatch('load')
+
+    expect(getTitres).toHaveBeenCalled()
+    expect(getTitre).not.toHaveBeenCalled()
+    expect(getUtilisateurs).not.toHaveBeenCalled()
+    expect(getUtilisateur).not.toHaveBeenCalled()
+  })
+
+  test('charge le router sur le titre', async () => {
+    router.push({ name: 'titre', params: { id: '123' } })
+    await store.dispatch('load')
+
+    expect(getTitres).not.toHaveBeenCalled()
+    expect(getTitre).toHaveBeenCalled()
+    expect(getUtilisateurs).not.toHaveBeenCalled()
+    expect(getUtilisateur).not.toHaveBeenCalled()
+  })
+
+  test('charge le router sur les utilisateurs', async () => {
+    router.push({ name: 'utilisateurs', params: { id: '123' } })
+    await store.dispatch('load')
+
+    expect(getTitres).not.toHaveBeenCalled()
+    expect(getTitre).not.toHaveBeenCalled()
+    expect(getUtilisateurs).toHaveBeenCalled()
+    expect(getUtilisateur).not.toHaveBeenCalled()
+  })
+
+  test('charge le router sur un utilisateur', async () => {
+    router.push({ name: 'utilisateur', params: { id: '123' } })
+    await store.dispatch('load')
+
+    expect(getTitres).not.toHaveBeenCalled()
+    expect(getTitre).not.toHaveBeenCalled()
+    expect(getUtilisateurs).not.toHaveBeenCalled()
+    expect(getUtilisateur).toHaveBeenCalled()
+  })
+
+  test("ecrit la version de l'api", () => {
+    const version = '10.4'
+    store.commit('apiVersionSet', version)
+
+    expect(state.versions.api).toEqual(version)
+  })
+
+  test("charge l'etat", () => {
+    store.commit('loaded')
+
+    expect(state.loaded).toBeTruthy()
+  })
+
+  test('ajoute un message', () => {
+    const message = { id: 42, message: 'message très important' }
+    store.commit('messageAdd', message)
+
+    expect(state.messages).toEqual([message])
+  })
+
+  test('supprime un message', () => {
+    const message = { id: 42, message: 'message très important' }
+    store.commit('messageAdd', message)
+    expect(state.messages).toEqual([message])
+    store.commit('messageRemove', 42)
+
+    expect(state.messages).toEqual([])
+  })
+
+  test('ouvre une pop up', () => {
+    const component = 'open'
+    const props = 'open'
+    store.commit('popupOpen', { component, props })
+
+    expect(state.popup).toEqual({
+      component: 'open',
+      props: 'open',
+      messages: []
+    })
+  })
+
+  test('ferme les pops up', () => {
+    const component = 'open'
+    const props = 'open'
+    store.commit('popupOpen', { component, props })
+    store.commit('popupClose')
+
+    expect(state.popup).toEqual({
+      component: null,
+      props: null,
+      messages: []
+    })
+  })
+
+  test('ajoute une erreur', () => {
+    const error = "erreur dans l'api"
+    store.commit('errorAdd', error)
+
+    expect(state.error).toEqual(error)
+  })
+
+  test('supprime les erreurs', () => {
+    const error = "erreur dans l'api"
+    store.commit('errorAdd', error)
+    store.commit('errorRemove')
+
+    expect(state.error).toBeNull()
+  })
+
+  test('ouvre le menu', () => {
+    const component = 'component'
+    store.commit('menuOpen', component)
+
+    expect(state.menu.component).toEqual(component)
+  })
+
+  test('ferme le menu', () => {
+    const component = 'component'
+    store.commit('menuOpen', component)
+    store.commit('menuClose')
+
+    expect(state.menu.component).toBeNull()
+  })
+
+  test('ajoute un message de popup', () => {
+    const message = 'message très important'
+    store.commit('popupMessageAdd', message)
+
+    expect(state.popup.messages).toEqual([message])
+  })
+
+  test('supprime les messages de popup', () => {
+    const message = 'message très important'
+    store.commit('popupMessageAdd', message)
+    store.commit('popupMessagesRemove')
+
+    expect(state.popup.messages).toEqual([])
+  })
+
+  test("charge le nom d'un element", () => {
+    const name = 'nom'
+    store.commit('loadingAdd', name)
+
+    expect(state.loading).toEqual([name])
+  })
+
+  test("supprime le nom d'un element", () => {
+    const name1 = 'nom1'
+    const name2 = 'nom2'
+    store.commit('loadingAdd', name1)
+    store.commit('loadingAdd', name2)
+    store.commit('loadingRemove', name1)
+
+    expect(state.loading).toEqual([name2])
+  })
+
+  test("supprime le nom d'un element n'existant pas", () => {
+    const name1 = 'nom1'
+    const name2 = 'nom2'
+    store.commit('loadingAdd', name1)
+    store.commit('loadingRemove', name2)
+
+    expect(state.loading).toEqual([name1])
   })
 })
--- src/store/user.test.js
+++ src/store/user.test.js
@@ -1,9 +1,9 @@
 import user from './user'
-import * as Vue from 'vue'
-import * as api from '../api/utilisateurs'
-import * as router from '../router'
+import * as api from '../api'
+import { createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 
-jest.mock('../api/utilisateurs', () => ({
+jest.mock('../api', () => ({
   utilisateurLogin: jest.fn(),
   utilisateurIdentify: jest.fn(),
   utilisateurPasswordInit: jest.fn(),
@@ -12,21 +12,412 @@ jest.mock('../api/utilisateurs', () => ({
   utilisateurAdd: jest.fn()
 }))
 
-jest.mock('vue', () => ({
-  set: jest.fn()
-}))
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 jest.mock('../router', () => [])
 
-describe('tests sur mutations', () => {
-  test('preferenceSet fonctionne', () => {
-    expect(true)
-    // const value = 'value'
-    // const state = { preferences: [3, 4, 5, 2] }
-    // const section = '2.3.4.5.7.80.156.665.5.59.1'
-    // const apiSpy = Vue.set.mockImplementation((path, id, value) => true)
-    // user.mutations.preferenceSet(state, { section, value })
-    // expect(apiSpy).toHaveBeenCalledTimes(1)
-    // expect(apiSpy).toHaveBeenCalledWith('state', 1, value)
+describe('teste user', () => {
+  let store
+  let actions
+  let mutations
+  let userInfo
+  let map
+  let email
+  let motDePasse
+  beforeEach(() => {
+    email = 'rene@la.taupe'
+    motDePasse = 'mignon'
+    userInfo = {
+      id: 66,
+      prenom: 'rene',
+      nom: 'lataupe',
+      permission: 'admin',
+      entreprise: 'macdo',
+      email: 'rene@la.taupe',
+      motDePasse: 'mignon'
+    }
+    user.state = {
+      current: null,
+      preferences: {
+        carte: { tilesId: 'osm-fr' },
+        filtres: {},
+        titres: { vueId: 'carte', pageActive: 1, pagesRange: 200 }
+      }
+    }
+    actions = {
+      load: jest.fn(),
+      messageAdd: jest.fn()
+    }
+    mutations = {
+      popupMessagesRemove: jest.fn(),
+      loadingAdd: jest.fn(),
+      popupClose: jest.fn(),
+      popupMessageAdd: jest.fn(),
+      loadingRemove: jest.fn(),
+      menuClose: jest.fn()
+    }
+    map = { state: { tiles: [{ id: 'osm-fr' }, { id: 'geoportail' }] } }
+    store = new Vuex.Store({ modules: { user, map }, actions, mutations })
+    localStorage.clear()
+  })
+
+  test("initialise l'user si un token existe", async () => {
+    localStorage.setItem('token', 'rene')
+    const apiMock = api.utilisateurIdentify.mockResolvedValue({
+      token: 'rene',
+      utilisateur: userInfo
+    })
+    store = new Vuex.Store({ modules: { user, map }, actions, mutations })
+    await store.dispatch('user/init')
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith()
+  })
+
+  test("n'initialise pas l'user si aucun token n'existe", async () => {
+    await store.dispatch('user/init')
+
+    expect(localStorage.getItem('token')).toBeNull()
+  })
+
+  test("identifie l'user", async () => {
+    const apiMock = api.utilisateurIdentify.mockResolvedValue({
+      token: 'rene',
+      utilisateur: userInfo
+    })
+    await store.dispatch('user/identify', { email, motDePasse })
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith()
+    expect(localStorage.getItem('token')).toEqual('rene')
+    expect(store.state.user.current).toEqual({
+      id: 66,
+      prenom: 'rene',
+      nom: 'lataupe',
+      permission: 'admin',
+      entreprise: 'macdo'
+    })
+  })
+
+  test("erreur de l'api lors de l'identification de l'user", async () => {
+    const apiMock = api.utilisateurIdentify.mockRejectedValue(
+      new Error("erreur dans l'api")
+    )
+    localStorage.setItem('token', 'rene')
+    store.commit('set', userInfo)
+    await store.dispatch('user/identify', { email, motDePasse })
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith()
+    expect(localStorage.getItem('token')).toBeNull()
+    expect(store.state.user.current).toBeNull()
+  })
+
+  test('login un user', async () => {
+    const apiMock = api.utilisateurLogin.mockResolvedValue({
+      token: 'rene',
+      utilisateur: userInfo
+    })
+    await store.dispatch('user/login', { email, motDePasse })
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ email, motDePasse })
+    expect(mutations.popupClose).toHaveBeenCalled()
+    expect(actions.messageAdd).toHaveBeenCalled()
+    expect(actions.load).toHaveBeenCalled()
+    expect(localStorage.getItem('token')).toEqual('rene')
+    expect(store.state.user.current).toEqual({
+      id: 66,
+      prenom: 'rene',
+      nom: 'lataupe',
+      permission: 'admin',
+      entreprise: 'macdo'
+    })
+  })
+
+  test("erreur de l'api lors du login d'un user", async () => {
+    localStorage.setItem('token', 'rene')
+    store.commit('set', userInfo)
+    const apiMock = api.utilisateurLogin.mockRejectedValue(
+      new Error("erreur dans l'api")
+    )
+    await store.dispatch('user/login', { email, motDePasse })
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ email, motDePasse })
+    expect(localStorage.getItem('token')).toBeNull()
+    expect(store.state.user.current).toBeNull()
+    expect(mutations.popupMessageAdd).toHaveBeenCalled()
+  })
+
+  test('logout un user', () => {
+    localStorage.setItem('token', 'value')
+    store.commit('user/set', userInfo)
+    store.dispatch('user/logout')
+
+    expect(mutations.menuClose).toHaveBeenCalled()
+    expect(actions.messageAdd).toHaveBeenCalled()
+    expect(actions.load).toHaveBeenCalled()
+    expect(localStorage.getItem('token')).toBeNull()
+    expect(store.state.user.current).toBeNull()
+  })
+
+  test('ajoute un email', async () => {
+    const apiMock = api.utilisateurAddEmail.mockResolvedValue(email)
+    await store.dispatch('user/addEmail', email)
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ email })
+    expect(mutations.popupClose).toHaveBeenCalled()
+    expect(actions.messageAdd).toHaveBeenCalled()
+  })
+
+  test("erreur de l'api lors de l'ajout d'un email", async () => {
+    const apiMock = api.utilisateurAddEmail.mockRejectedValue(
+      new Error("erreur dans l'api")
+    )
+    await store.dispatch('user/addEmail', email)
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ email })
+    expect(mutations.popupClose).not.toHaveBeenCalled()
+    expect(mutations.popupMessageAdd).toHaveBeenCalled()
+  })
+
+  test('ajoute un user', async () => {
+    const loginMock = jest.fn()
+    user.actions.login = loginMock
+    store = new Vuex.Store({ modules: { user, map }, actions, mutations })
+    const apiMock = api.utilisateurAdd.mockResolvedValue(userInfo)
+    await store.dispatch('user/add', userInfo)
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ utilisateur: userInfo })
+    expect(actions.messageAdd).toHaveBeenCalled()
+    expect(loginMock).toHaveBeenCalled()
+  })
+
+  test("n'ajoute pas d'utilisateur", async () => {
+    const apiMock = api.utilisateurAdd.mockResolvedValue(null)
+    await store.dispatch('user/add', userInfo)
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ utilisateur: userInfo })
+    expect(actions.messageAdd).not.toHaveBeenCalled()
+  })
+
+  test("erreur api lors de l'ajout d'un user", async () => {
+    const loginMock = jest.fn()
+    user.actions.login = loginMock
+    store = new Vuex.Store({ modules: { user, map }, actions, mutations })
+    const apiMock = api.utilisateurAdd.mockRejectedValue(
+      new Error("erreur dans l'api")
+    )
+    await store.dispatch('user/add', userInfo)
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ utilisateur: userInfo })
+    expect(actions.messageAdd).toHaveBeenCalled()
+    expect(loginMock).not.toHaveBeenCalled()
+  })
+
+  test("inite l'email d'un user", async () => {
+    const apiMock = api.utilisateurPasswordInitEmail.mockResolvedValue(userInfo)
+    await store.dispatch('user/passwordInitEmail', email)
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ email })
+    expect(mutations.popupClose).toHaveBeenCalled()
+    expect(actions.messageAdd).toHaveBeenCalled()
+  })
+
+  test("erreur api dans la création de l'email de l'user", async () => {
+    const apiMock = api.utilisateurPasswordInitEmail.mockRejectedValue(
+      new Error("erreur dans l'api")
+    )
+    await store.dispatch('user/passwordInitEmail', email)
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ email })
+    expect(mutations.popupMessageAdd).toHaveBeenCalled()
+    expect(actions.messageAdd).not.toHaveBeenCalled()
+  })
+
+  test("initie le mot de passe d'un user", async () => {
+    const loginMock = jest.fn()
+    user.actions.login = loginMock
+    store = new Vuex.Store({ modules: { user, map }, actions, mutations })
+    const apiMock = api.utilisateurPasswordInit.mockResolvedValue(userInfo)
+    const res = await store.dispatch('user/passwordInit', {
+      motDePasse1: motDePasse,
+      motDePasse2: motDePasse,
+      email
+    })
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({
+      motDePasse1: motDePasse,
+      motDePasse2: motDePasse
+    })
+    expect(actions.messageAdd).toHaveBeenCalled()
+    expect(loginMock).toHaveBeenCalled()
+    expect(res).toEqual(userInfo)
+  })
+
+  test("erreur api dans la création du mot de passe de l'user", async () => {
+    const motDePasse1 = 'mignon'
+    const motDePasse2 = 'mignon'
+    const loginMock = jest.fn()
+    user.actions.login = loginMock
+    store = new Vuex.Store({ modules: { user, map }, actions, mutations })
+    const apiMock = api.utilisateurPasswordInit.mockRejectedValue(
+      new Error("erreur dans l'api")
+    )
+    const res = await store.dispatch('user/passwordInit', {
+      motDePasse1,
+      motDePasse2,
+      email
+    })
+
+    expect(apiMock).toHaveBeenCalled()
+    expect(apiMock).toHaveBeenCalledWith({ motDePasse1, motDePasse2 })
+    expect(actions.messageAdd).toHaveBeenCalled()
+    expect(loginMock).not.toHaveBeenCalled()
+    expect(res).toBeUndefined()
+  })
+
+  test("set les preferences de l'utilisateur sans conditions", async () => {
+    const section = 'filtres.domaines'
+    const value = 'c,w'
+    await store.dispatch('user/preferenceSet', { section, value })
+
+    expect(store.state.user.preferences.filtres.domaines).toEqual(value)
+    expect(localStorage.getItem('conditions')).toBeNull()
+  })
+
+  test("set les preferences de l'utilisateur avec conditions", async () => {
+    const section = 'conditions'
+    const value = 'conditionValue'
+    await store.dispatch('user/preferenceSet', { section, value })
+
+    expect(store.state.user.preferences.conditions).toEqual(value)
+    expect(localStorage.getItem('conditions')).toEqual(value)
+  })
+
+  test('reset les preferences', async () => {
+    const section = 'filtres.domaines'
+    const value = 'c,w'
+    await store.dispatch('user/preferenceSet', { section, value })
+    await store.dispatch('user/preferenceReset', { section })
+
+    expect(store.state.user.preferences.filtres.domaines).toBeUndefined()
+  })
+
+  test('cree le token', async () => {
+    await store.dispatch('user/tokenSet', 'value')
+
+    expect(localStorage.getItem('token')).toEqual('value')
+  })
+
+  test('enleve le token', async () => {
+    localStorage.setItem('token', 'value')
+    await store.dispatch('user/tokenRemove')
+
+    expect(localStorage.getItem('token')).toBeNull()
+  })
+
+  test('regarde si la tuile active appartient aux tuiles de la carte', () => {
+    expect(store.getters['user/tilesActive']).toEqual({ id: 'osm-fr' })
+  })
+
+  test("la tuile active n'appartient pas aux tuiles de la carte", () => {
+    map = { state: { tiles: [{ id: 'geoportail' }] } }
+    store = new Vuex.Store({ modules: { user, map }, actions, mutations })
+
+    expect(store.getters['user/tilesActive']).toBeUndefined()
+  })
+
+  test("teste si l'user est connecté", () => {
+    user.state.current = {}
+    store = new Vuex.Store({ modules: { user } })
+
+    expect(store.getters['user/preferencesConditions']).toBeTruthy()
+  })
+
+  test('pas de préferences sauvegardées', () => {
+    expect(store.getters['user/preferencesConditions']).toBeFalsy()
+  })
+
+  test('des preferences sauvegardées existent mais sont vieilles de plus de 3 jours', () => {
+    localStorage.setItem('conditions', '3')
+    expect(store.getters['user/preferencesConditions']).toBeFalsy()
+  })
+
+  test('des preferences sauvegardées existent depuis moins 3 jours', () => {
+    localStorage.setItem('conditions', new Date().getTime().toString())
+    expect(store.getters['user/preferencesConditions']).toBeTruthy()
+  })
+
+  test("set les preferences de l'user", () => {
+    const section = 'filtres.domaines'
+    const value = 'c,w'
+    store.commit('user/preferenceSet', { section, value })
+
+    expect(store.state.user.preferences.filtres.domaines).toBeDefined()
+    expect(store.state.user.preferences.filtres.domaines).toEqual(value)
+  })
+
+  test("reset les preferences de l'user", () => {
+    const section = 'filtres.domaines'
+    const value = 'c,w'
+    store.commit('user/preferenceSet', { section, value })
+    expect(store.state.user.preferences.filtres.domaines).toEqual(value)
+    store.commit('user/preferenceReset', { section })
+
+    expect(store.state.user.preferences.filtres.domaines).toBeUndefined()
+  })
+
+  test("set le statut de l'user avec une entreprise", () => {
+    store.commit('user/set', userInfo)
+
+    expect(store.state.user.current).toEqual({
+      id: 66,
+      prenom: 'rene',
+      nom: 'lataupe',
+      permission: 'admin',
+      entreprise: 'macdo'
+    })
+  })
+
+  test("set le statut de l'user sans entreprises", () => {
+    store.commit('user/set', {
+      id: 66,
+      prenom: 'rene',
+      nom: 'lataupe',
+      permission: 'admin'
+    })
+
+    expect(store.state.user.current).toEqual({
+      id: 66,
+      prenom: 'rene',
+      nom: 'lataupe',
+      permission: 'admin'
+    })
+    expect(store.state.user.current.entreprise).toBeUndefined()
+  })
+
+  test("reset le statut de l'user", () => {
+    store.commit('user/set', userInfo)
+    expect(store.state.user.current).toEqual({
+      id: 66,
+      prenom: 'rene',
+      nom: 'lataupe',
+      permission: 'admin',
+      entreprise: 'macdo'
+    })
+    store.commit('user/reset')
+
+    expect(store.state.user.current).toBeNull()
   })
 })
