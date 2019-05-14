cc64e1c2fd043eff2eb87640e1c8d02f78f84a7c
Florent Chalvet <florentchalvet@gmail.com>
2019-05-14 12:24:40 +0200

    test: modify index

--- src/store/index.test.js
+++ src/store/index.test.js
@@ -4,8 +4,6 @@ import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import * as api from '../api'
 
-jest.mock('../router', () => ({}))
-
 jest.mock('../api', () => ({ init: jest.fn() }))
 jest.mock('./titre', () => ({ titre: jest.fn() }))
 jest.mock('./titres', () => ({ titres: jest.fn() }))
@@ -29,9 +27,9 @@ jest.useFakeTimers()
 describe('teste les fonctions utilisées dans les autres scripts', () => {
   let state
   let store
-  let setMetas, getTitre, getTitres, getUtilisateurs, getUtilisateur
-  let permissionsSetUtilisateurs, setSubstances, setEntreprises
-  let router
+  let modules
+  let setMetas, permissionsSetUtilisateurs, setSubstances, setEntreprises
+
   beforeEach(() => {
     state = {
       config: {},
@@ -44,195 +42,32 @@ describe('teste les fonctions utilisées dans les autres scripts', () => {
         ui: `${npmVersion}`
       },
       loading: [],
-      loaded: false
+      loaded: false,
+      route: null
     }
     setMetas = jest.fn()
-    getTitre = jest.fn()
-    getTitres = jest.fn()
-    getUtilisateurs = jest.fn()
-    getUtilisateur = jest.fn()
     permissionsSetUtilisateurs = jest.fn()
     setSubstances = jest.fn()
     setEntreprises = jest.fn()
-    router = new VueRouter({
-      routes: [
-        {
-          path: '/titres',
-          name: 'titres'
-        },
-        {
-          path: '/titres/:id',
-          name: 'titre'
-        },
-        {
-          path: '/utilisateurs/:id',
-          name: 'utilisateur'
-        },
-        {
-          path: '/utilisateurs',
-          name: 'utilisateurs'
-        }
-      ]
-    })
-    store = new Vuex.Store({
-      modules: {
-        titre: { namespaced: true, actions: { get: getTitre } },
-        titres: { namespaced: true, actions: { get: getTitres } },
-        metas: { namespaced: true, actions: { set: setMetas } },
-        utilisateur: { namespaced: true, actions: { get: getUtilisateur } },
-        utilisateurs: {
-          namespaced: true,
-          actions: { get: getUtilisateurs },
-          mutations: { permissionsSet: permissionsSetUtilisateurs }
-        },
-        entreprises: { namespaced: true, mutations: { set: setEntreprises } },
-        substances: { namespaced: true, mutations: { set: setSubstances } }
+
+    modules = {
+      metas: { namespaced: true, actions: { set: setMetas } },
+      utilisateurs: {
+        namespaced: true,
+        mutations: { permissionsSet: permissionsSetUtilisateurs }
       },
+      entreprises: { namespaced: true, mutations: { set: setEntreprises } },
+      substances: { namespaced: true, mutations: { set: setSubstances } }
+    }
+
+    store = new Vuex.Store({
+      modules,
       state,
       actions,
       mutations
     })
+    localStorage.clear()
   })
-
-  test('initie tout les différents types de composants', async () => {
-    const apiMock = api.init.mockResolvedValue({
-      version: '10.4',
-      metas: { domaines: 'c,w', statuts: 'val' },
-      substances: 'or',
-      permissions: 'admin',
-      entreprises: 'macdo'
-    })
-    await store.dispatch('init')
-
-    expect(state.versions.api).toEqual('10.4')
-    expect(permissionsSetUtilisateurs).toHaveBeenCalled()
-    expect(setSubstances).toHaveBeenCalled()
-    expect(setEntreprises).toHaveBeenCalled()
-    expect(setMetas).toHaveBeenCalled()
-  })
-
-  test('initie sans composants', async () => {
-    const apiMock = api.init.mockResolvedValue({})
-    await store.dispatch('init')
-
-    expect(state.versions.api).toBeNull()
-    expect(permissionsSetUtilisateurs).not.toHaveBeenCalled()
-    expect(setSubstances).not.toHaveBeenCalled()
-    expect(setEntreprises).not.toHaveBeenCalled()
-    expect(setMetas).not.toHaveBeenCalled()
-  })
-
-  test("erreur dans l'api lors de l'initiation", async () => {
-    const apiMock = api.init.mockRejectedValue(new Error("erreur dans l'api"))
-    await store.dispatch('init')
-
-    expect(state.messages.pop().type).toEqual('error')
-    expect(console.log).toHaveBeenCalled()
-  })
-
-  test("renvoie une erreur definie de l'api", async () => {
-    const error = "erreur dans l'api"
-    await store.dispatch('apiError', error)
-
-    expect(state.messages).toEqual([
-      { type: 'error', value: "erreur dans l'api" }
-    ])
-  })
-
-  test("renvoie une erreur indefinie de l'api", async () => {
-    await store.dispatch('apiError')
-
-    expect(state.messages).toEqual([
-      {
-        type: 'error',
-        value: "Erreur: impossible de se connecter à l'API (undefined)"
-      }
-    ])
-  })
-
-  test('renvoie une erreur de la page', async () => {
-    await store.dispatch('pageError')
-
-    expect(state.error).toEqual({
-      type: 'error',
-      value: `Erreur: page introuvable`
-    })
-  })
-
-  test('ajoute un message et le supprime plus tard', async () => {
-    const message = { id: 14, message: 'message important' }
-    await store.dispatch('messageAdd', message)
-
-    const res = state.messages.pop()
-    expect(res.message).toEqual('message important')
-    expect(res.id).toBeLessThanOrEqual(new Date().valueOf())
-    expect(setTimeout).toHaveBeenCalled()
-    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 4500)
-  })
-
-  test('ferme le menu deja ouvert', async () => {
-    store.state.menu.component = { name: 'bonjour' }
-    const component = { name: 'bonjour' }
-    await store.dispatch('menuToggle', component)
-
-    expect(state.menu.component).toBeNull()
-  })
-
-  test('ferme le menu pour en rouvrir un autre', async () => {
-    store.state.menu.component = { name: 'bonjour' }
-    const component = { name: 'hello' }
-    expect(state.menu.component).toEqual({ name: 'bonjour' })
-    await store.dispatch('menuToggle', component)
-
-    expect(state.menu.component).toEqual(component)
-  })
-
-  test('ouvre un nouveau menu', async () => {
-    const component = { name: 'hello' }
-    await store.dispatch('menuToggle', component)
-
-    expect(state.menu.component).toEqual(component)
-  })
-
-  // test('charge le router sur les titres', async () => {
-  //   router.push({ name: 'titres' })
-  //   await store.dispatch('load')
-
-  //   expect(getTitres).toHaveBeenCalled()
-  //   expect(getTitre).not.toHaveBeenCalled()
-  //   expect(getUtilisateurs).not.toHaveBeenCalled()
-  //   expect(getUtilisateur).not.toHaveBeenCalled()
-  // })
-
-  // test('charge le router sur le titre', async () => {
-  //   router.push({ name: 'titre', params: { id: '123' } })
-  //   await store.dispatch('load')
-
-  //   expect(getTitres).not.toHaveBeenCalled()
-  //   expect(getTitre).toHaveBeenCalled()
-  //   expect(getUtilisateurs).not.toHaveBeenCalled()
-  //   expect(getUtilisateur).not.toHaveBeenCalled()
-  // })
-
-  // test('charge le router sur les utilisateurs', async () => {
-  //   router.push({ name: 'utilisateurs' })
-  //   await store.dispatch('load')
-
-  //   expect(getTitres).not.toHaveBeenCalled()
-  //   expect(getTitre).not.toHaveBeenCalled()
-  //   expect(getUtilisateurs).toHaveBeenCalled()
-  //   expect(getUtilisateur).not.toHaveBeenCalled()
-  // })
-
-  // test('charge le router sur un utilisateur', async () => {
-  //   router.push({ name: 'utilisateur', params: { id: '123' } })
-  //   await store.dispatch('load')
-
-  //   expect(getTitres).not.toHaveBeenCalled()
-  //   expect(getTitre).not.toHaveBeenCalled()
-  //   expect(getUtilisateurs).not.toHaveBeenCalled()
-  //   expect(getUtilisateur).toHaveBeenCalled()
-  // })
 
   test("ecrit la version de l'api", () => {
     const version = '10.4'
@@ -289,14 +124,14 @@ describe('teste les fonctions utilisées dans les autres scripts', () => {
   })
 
   test('ajoute une erreur', () => {
-    const error = "erreur dans l'api"
+    const error = 'erreur api'
     store.commit('errorAdd', error)
 
     expect(state.error).toEqual(error)
   })
 
   test('supprime les erreurs', () => {
-    const error = "erreur dans l'api"
+    const error = 'erreur api'
     store.commit('errorAdd', error)
     store.commit('errorRemove')
 
@@ -357,5 +192,134 @@ describe('teste les fonctions utilisées dans les autres scripts', () => {
     store.commit('loadingRemove', name2)
 
     expect(state.loading).toEqual([name1])
+  })
+
+  // test('documentDownload', async () => {
+  //   const titreDocumentId = 'crique-sophie'
+  //   const fileName = 'criqueSophie'
+  //   jest.spyOn(global, 'fetch').mockResolvedValue(
+  //     Promise.resolve({
+  //       json: () => Promise.resolve({})
+  //     })
+  //   )
+  //   localStorage.setItem(
+  //     'token',
+  //     'privateToken'
+  //   )
+  //   await store.dispatch('documentDownload', { titreDocumentId, fileName })
+
+  //   expect(global.fetch).toHaveBeenCalled()
+  // })
+
+  test('initie tout les différents types de composants', async () => {
+    const errorRemoveMock = jest.fn()
+    mutations.errorRemove = errorRemoveMock
+    const apiVersionSetMock = jest.fn()
+    mutations.apiVersionSet = apiVersionSetMock
+    store = new Vuex.Store({ modules, state, actions, mutations })
+    api.init.mockResolvedValue({
+      version: '10.4',
+      metas: { domaines: 'c,w', statuts: 'val' },
+      substances: 'or',
+      permissions: 'admin',
+      entreprises: 'macdo'
+    })
+    await store.dispatch('init')
+
+    expect(errorRemoveMock).toHaveBeenCalled()
+    expect(apiVersionSetMock).toHaveBeenCalled()
+    expect(permissionsSetUtilisateurs).toHaveBeenCalled()
+    expect(setSubstances).toHaveBeenCalled()
+    expect(setEntreprises).toHaveBeenCalled()
+    expect(setMetas).toHaveBeenCalled()
+  })
+
+  test('initie sans composants', async () => {
+    api.init.mockResolvedValue({})
+    await store.dispatch('init')
+
+    expect(state.versions.api).toBeNull()
+    expect(permissionsSetUtilisateurs).not.toHaveBeenCalled()
+    expect(setSubstances).not.toHaveBeenCalled()
+    expect(setEntreprises).not.toHaveBeenCalled()
+    expect(setMetas).not.toHaveBeenCalled()
+  })
+
+  test("erreur api lors de l'initiation", async () => {
+    api.init.mockRejectedValue(new Error('erreur api'))
+    await store.dispatch('init')
+
+    expect(state.messages.pop().type).toEqual('error')
+    expect(console.log).toHaveBeenCalled()
+  })
+
+  test("renvoie une erreur definie de l'api", async () => {
+    Date.now = jest.fn(() => 1487076708000)
+    const error = 'erreur api'
+    await store.dispatch('apiError', error)
+
+    expect(state.messages).toEqual([
+      { id: 1487076708000, type: 'error', value: 'erreur api' }
+    ])
+  })
+
+  test("renvoie une erreur indefinie de l'api", async () => {
+    Date.now = jest.fn(() => 1487076708000)
+    process.env.VUE_APP_API_URL = 'http://api.camino-test-url.dev'
+    await store.dispatch('apiError')
+
+    expect(state.messages).toEqual([
+      {
+        id: 1487076708000,
+        type: 'error',
+        value:
+          "Erreur: impossible de se connecter à l'API (http://api.camino-test-url.dev)"
+      }
+    ])
+  })
+
+  test('renvoie une erreur de la page', async () => {
+    await store.dispatch('pageError')
+
+    expect(state.error).toEqual({
+      type: 'error',
+      value: `Erreur: page introuvable`
+    })
+  })
+
+  test('ajoute un message et le supprime plus tard', async () => {
+    const message = { id: 14, message: 'message important' }
+    await store.dispatch('messageAdd', message)
+
+    const res = state.messages.pop()
+    expect(res.message).toEqual('message important')
+    expect(res.id).toBeLessThanOrEqual(new Date().valueOf())
+    expect(setTimeout).toHaveBeenCalled()
+    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 4500)
+  })
+
+  test('ferme le menu deja ouvert', async () => {
+    store.state.menu.component = { name: 'menu' }
+    const component = { name: 'menu' }
+
+    await store.dispatch('menuToggle', component)
+
+    expect(state.menu.component).toBeNull()
+  })
+
+  test('ferme le menu pour en rouvrir un autre', async () => {
+    store.state.menu.component = { name: 'bonjour' }
+    const component = { name: 'hello' }
+
+    await store.dispatch('menuToggle', component)
+
+    expect(state.menu.component).toEqual(component)
+  })
+
+  test('ouvre un nouveau menu', async () => {
+    const component = { name: 'hello' }
+    await store.dispatch('menuToggle', component)
+
+    expect(state.menu.component).toEqual(component)
   })
 })
