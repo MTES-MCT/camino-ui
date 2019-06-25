4ffef3c3247ab7f6eb949ca3b4a7e286f52f09c7
Florent Chalvet <florentchalvet@gmail.com>
2019-06-25 15:56:20 +0200

    fix: Modifie les arrays en string

--- src/components/titres/geojson-download.vue
+++ src/components/titres/geojson-download.vue
@@ -57,26 +57,29 @@ export default {
               statut: titre.statut.nom,
               volume: titre.volume,
               volume_unite: titre.volumeUnite,
-              substances: titre.substances.map(s => s.nom),
+              substances: titre.substances.map(s => s.nom).join(', ') || null,
               surface_km2: titre.surface,
               administrations_noms: titre.administrations.map(a => a.nom),
-              titulaires_noms: titre.titulaires.map(e => e.nom),
-              titulaires_legal: titre.titulaires.map(
-                e => e.legalEtranger || e.legalSiren
-              ),
-              amodiataires_noms: titre.amodiataires.map(e => e.nom),
-              amodiataires_legal: titre.amodiataires.map(
-                e => e.legalEtranger || e.legalSiren
-              ),
+              titulaires_noms:
+                titre.titulaires.map(e => e.nom).join(', ') || null,
+              titulaires_legal:
+                titre.titulaires
+                  .map(e => e.legalEtranger || e.legalSiren)
+                  .join(', ') || null,
+              amodiataires_noms:
+                titre.amodiataires.map(e => e.nom).join(', ') || null,
+              amodiataires_legal:
+                titre.amodiataires
+                  .map(e => e.legalEtranger || e.legalSiren)
+                  .join(', ') || null,
               engagement: titre.engagement,
               engagement_devise:
                 titre.engagementDevise && titre.engagementDevise.id,
               references:
                 titre.references &&
-                titre.references.reduce((obj, reference) => {
-                  obj[reference.type] = reference.valeur
-                  return obj
-                }, {})
+                titre.references
+                  .map(reference => `${reference.type}: ${reference.valeur}`)
+                  .join(', ')
             },
             type: 'Feature',
             geometry: titre.geojsonMultiPolygon.geometry
