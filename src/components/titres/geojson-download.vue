4905d9b2470cdd30dad9e931b0a328f9c89f4761
Adrien Risser <risseraka@gmail.com>
2019-07-01 14:24:16 +0200

    fix(export): corrige le format de la date dans le nom du fichier (#21)

--- src/components/titres/geojson-download.vue
+++ src/components/titres/geojson-download.vue
@@ -20,12 +20,12 @@ export default {
       const titresGeojson = this.titresFormatGeojson(this.titres)
       const name = (() => {
         const d = new Date()
-        const dd = d.getDate()
-        const mm = d.getMonth() + 1
+        const dd = d.getDate().toString().padStart(2, '0')
+        const mm = (d.getMonth() + 1).toString().padStart(2, '0')
         const yyyy = d.getFullYear()
-        const hh = d.getHours()
-        const mi = d.getMinutes()
-        return `${yyyy}${mm}${dd}-${hh}-${mi}-camino-export.geojson`
+        const hh = d.getHours().toString().padStart(2, '0')
+        const mi = d.getMinutes().toString().padStart(2, '0')
+        return `${yyyy}${mm}${dd}-${hh}h${mi}-camino-titre-export.csv`
       })()
 
       const link = document.createElement('a')
