b70bf453c062b42aaf055b2d1489712dc710212e
Francois Romain <francoisromain@gmail.com>
2019-10-03 18:32:10 +0200

    feat: sélectionne le système géographique globalement

--- src/components/titre/points.vue
+++ src/components/titre/points.vue
@@ -1,66 +1,105 @@
 <template>
   <div class="points">
-    <div
-      v-for="(groupeContours, groupeIndex) in groupes"
-      :key="groupeIndex + 1"
-      class="geo-groupe mb"
-    >
-      <h4
-        v-if="groupes.length > 1"
-        class="color-bg pl-s mb-s"
+    <h4 class="mb-s">
+      Systèmes géographiques
+    </h4>
+    <div class="full-x mb">
+      <select
+        v-if="geoSystemes.length > 1"
+        v-model="geoSystemeId"
+        type="text"
+        class="p-s mr-s"
       >
-        Groupe {{ groupeIndex + 1 }}
-      </h4>
+        <option
+          v-for="geoSysteme in geoSystemes"
+          :key="geoSysteme.id"
+          :value="geoSysteme.id"
+        >
+          {{ geoSysteme.nom }} ({{ geoSysteme.id }}) {{ geoSysteme.opposable ? '(opposable)' : '' }}
+        </option>
+      </select>
       <div
-        v-for="(contourPoints, contourIndex) in groupeContours"
-        :key="contourIndex + 1"
-        class="geo-contour"
+        v-else
+        class="full-x p-s bg-alt"
+      >
+        {{ geoSystemes[0].nom }} ({{ geoSystemes[0].id }})
+      </div>
+    </div>
+
+    <div
+      v-if="geoSystemeId"
+    >
+      <div class="tablet-blobs px-l">
+        <div class="tablet-blob-1-2" />
+        <div class="tablet-blob-1-2">
+          <div class="blobs">
+            <div class="blob-1-2">
+              <p class=" border-l pl-s">
+                X
+              </p>
+            </div>
+            <div class="blob-1-2">
+              <p class=" border-l pl-s">
+                Y
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        v-for="(groupeContours, groupeIndex) in groupes"
+        :key="groupeIndex + 1"
+        class="geo-groupe mb"
       >
         <h4
-          v-if="groupeContours.length > 1"
-          class="pl-s mb-s"
+          v-if="groupes.length > 1"
+          class="color-bg pl-s mb-s"
         >
-          {{ contourIndex === 0 ? 'Contour' : `Lacune ${contourIndex}` }}
+          Groupe {{ groupeIndex + 1 }}
         </h4>
         <div
-          v-for="(point) in contourPoints"
-          :key="point.id"
-          class="geo-point flex"
+          v-for="(contourPoints, contourIndex) in groupeContours"
+          :key="contourIndex + 1"
+          class="geo-contour"
         >
-          <h4 class="mb-s flex-self-start mr-s">
-            {{ point.nom }}
+          <h4
+            v-if="groupeContours.length > 1"
+            class="pl-s mb-s"
+          >
+            {{ contourIndex === 0 ? 'Contour' : `Lacune ${contourIndex}` }}
           </h4>
-          <div class="flex-grow">
-            <div
-              v-for="reference in point.references"
-              :key="reference.id"
-              class="tablet-blobs"
-            >
+          <div
+            v-for="(point) in contourPoints"
+            :key="point.id"
+            class="geo-point"
+          >
+            <div class="tablet-blobs">
+              <div class="tablet-blob-1-2 flex">
+                <h4 class="mb-0 flex-self-start mr-s">
+                  {{ point.nom }}
+                </h4>
+                <p
+                  v-if="point.description"
+                  class="mb-0 h5 flex-grow pt-xs"
+                >
+                  {{ point.description }}
+                </p>
+              </div>
               <div class="tablet-blob-1-2">
-                <p class="h6 mb-s mt-xs inline-block">
-                  <span class="bold">{{ reference.geoSysteme.nom }}</span>  ({{ reference.unite.nom }}) <span
-                    v-if="reference.opposable"
-                    class="h5 bg-info py-xxs px-xs rnd-xs color-bg"
-                  >Opposable</span>
-                </p>
-              </div>
-              <div class="tablet-blob-1-4">
-                <p class="h5 flex mb-s mt-xs">
-                  X : <span class="flex-right mono bold">{{ reference.coordonnees.x }}</span>
-                </p>
-              </div>
-              <div class="tablet-blob-1-4">
-                <p class="h5 flex mb-s mt-xs">
-                  Y : <span class="flex-right mono bold">{{ reference.coordonnees.y }}</span>
-                </p>
+                <div class="blobs">
+                  <div class="blob-1-2">
+                    <p class="h5 flex mb-s mt-xs border-l">
+                      <span class="flex-right mono bold">{{ Math.round(point.references[geoSystemeId].coordonnees.x * 1000000) / 1000000 }}</span>
+                    </p>
+                  </div>
+                  <div class="blob-1-2">
+                    <p class="h5 flex mb-s mt-xs border-l">
+                      <span class="flex-right mono bold">{{ Math.round(point.references[geoSystemeId].coordonnees.y * 1000000) / 1000000 }}</span>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-            <p
-              v-if="point.description"
-              class="mb-s h5"
-            >
-              {{ point.description }}
-            </p>
           </div>
         </div>
       </div>
@@ -77,6 +116,10 @@ export default {
     }
   },
 
+  data() {
+    return { geoSystemeId: '' }
+  },
+
   computed: {
     groupes() {
       return this.points.reduce(
@@ -91,7 +134,11 @@ export default {
             id,
             nom,
             description,
-            references
+            references: references.reduce((res, r) => {
+              res[r.geoSysteme.id] = r
+
+              return res
+            }, {})
           }
 
           return groupes
@@ -101,24 +148,39 @@ export default {
     },
 
     geoSystemes() {
-      return this.points.reduce((geoSystemes, { references }) => {
-        const pointGeoSystemes = references.reduce(
-          (pointGeoSystemes, { geoSysteme, unite, coordonnees }) => {
-            pointGeoSystemes[geoSysteme.id] = {
-              id: geoSysteme.id,
-              nom: geoSysteme.nom,
-              uniteId: unite.id,
-              uniteType: unite.type
-            }
+      const geoSystemesObject = this.points.reduce(
+        (geoSystemes, { references }) => {
+          const pointGeoSystemes = references.reduce(
+            (
+              pointGeoSystemes,
+              { geoSysteme, unite, coordonnees, opposable }
+            ) => {
+              pointGeoSystemes[geoSysteme.id] = {
+                id: geoSysteme.id,
+                nom: geoSysteme.nom,
+                uniteId: unite.id,
+                uniteType: unite.type,
+                opposable
+              }
 
-            return pointGeoSystemes
-          },
-          {}
-        )
+              return pointGeoSystemes
+            },
+            {}
+          )
 
-        return Object.assign(geoSystemes, pointGeoSystemes)
-      }, {})
+          return Object.assign(geoSystemes, pointGeoSystemes)
+        },
+        {}
+      )
+
+      return Object.keys(geoSystemesObject).map(
+        geoSystemeId => geoSystemesObject[geoSystemeId]
+      )
     }
+  },
+
+  created() {
+    this.geoSystemeId = this.geoSystemes[0].id
   }
 }
 </script>
