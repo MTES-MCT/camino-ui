f60d92057c71cc4c562d52b67864afe26fda27e8
Francois Romain <francoisromain@gmail.com>
2019-10-28 17:04:28 +0100

    fix: classe les entreprises et les administrations par ordre alphabétique

--- src/store/administrations.js
+++ src/store/administrations.js
@@ -27,19 +27,12 @@ export const actions = {
 
 export const getters = {}
 
-const stringCompare = (a, b) => (a > b ? 1 : a < b ? -1 : 0)
-
 export const mutations = {
   set(state, administrations) {
     Vue.set(
       state,
       'list',
-      administrations.sort((a, b) => {
-        if (!a.nom && !b.nom) return stringCompare(a, b)
-        if (!a.nom && b.nom) return 1
-        if (a.nom && !b.nom) return -1
-        return stringCompare(a.nom, b.nom)
-      })
+      administrations.sort((a, b) => a.nom.localeCompare(b.nom, 'fr'))
     )
   }
 }
--- src/store/entreprises.js
+++ src/store/entreprises.js
@@ -27,19 +27,12 @@ export const actions = {
 
 export const getters = {}
 
-const stringCompare = (a, b) => (a > b ? 1 : a < b ? -1 : 0)
-
 export const mutations = {
   set(state, entreprises) {
     Vue.set(
       state,
       'list',
-      entreprises.sort((a, b) => {
-        if (!a.nom && !b.nom) return stringCompare(a, b)
-        if (!a.nom && b.nom) return 1
-        if (a.nom && !b.nom) return -1
-        return stringCompare(a.nom, b.nom)
-      })
+      entreprises.sort((a, b) => a.nom.localeCompare(b.nom, 'fr'))
     )
   }
 }
