2efd5badc7a9a9307ee9fa35156174d3ef2aa9dd
Francois Romain <francoisromain@gmail.com>
2020-01-29 15:38:00 +0100

    fix: affiche le bouton de suppression d'un fichier

--- src/components/titre/document/edit.vue
+++ src/components/titre/document/edit.vue
@@ -71,7 +71,6 @@
         >
           <span class="h5">{{ document.fichierNouveau && document.fichierNouveau.name || `${document.id}.${document.fichierTypeId}` }}</span>
           <button
-            v-if="document.supprimable"
             class="btn-border py-s px-m my--xs rnd-xs flex-right"
             @click="fileRemove"
           >
