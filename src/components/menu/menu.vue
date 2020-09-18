db2f1bfc88ea5756505fc71803393f044dedbfae
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-09-18 14:13:22 +0200

    désactive l'item 'Statistique' du menu lorsque ses onglets sont séléectionnés

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -14,7 +14,10 @@
               >
                 Titres miniers et autorisations
               </RouterLink>
-              <div v-else class="bold color-neutral">
+              <div
+                v-else
+                class="bold color-neutral"
+              >
                 Titres miniers et autorisations
               </div>
             </li>
@@ -28,13 +31,19 @@
               >
                 Démarches
               </RouterLink>
-              <div v-else class="bold color-neutral">
+              <div
+                v-else
+                class="bold color-neutral"
+              >
                 Démarches
               </div>
             </li>
           </ul>
         </div>
-        <div v-if="sections.activites" class="tablet-blob-1-4 border-l pl-s">
+        <div
+          v-if="sections.activites"
+          class="tablet-blob-1-4 border-l pl-s"
+        >
           <ul class="list-sans mb-0">
             <li v-if="sections.activites">
               <RouterLink
@@ -46,7 +55,10 @@
               >
                 Activités
               </RouterLink>
-              <div v-else class="bold color-neutral">
+              <div
+                v-else
+                class="bold color-neutral"
+              >
                 Activités
               </div>
             </li>
@@ -64,7 +76,10 @@
               >
                 Entreprises
               </RouterLink>
-              <div v-else class="bold color-neutral">
+              <div
+                v-else
+                class="bold color-neutral"
+              >
                 Entreprises
               </div>
             </li>
@@ -78,13 +93,18 @@
               >
                 Utilisateurs
               </RouterLink>
-              <div v-else class="bold color-neutral">
+              <div
+                v-else
+                class="bold color-neutral"
+              >
                 Utilisateurs
               </div>
             </li>
           </ul>
         </div>
-        <div class="tablet-blob-1-4 border-l pl-s">
+        <div
+          class="tablet-blob-1-4 border-l pl-s"
+        >
           <ul class="list-sans mb-0">
             <li>
               <RouterLink
@@ -96,17 +116,18 @@
               >
                 Glossaire
               </RouterLink>
-              <div v-else class="bold color-neutral">
+              <div
+                v-else
+                class="bold color-neutral"
+              >
                 Glossaire
               </div>
             </li>
             <li>
               <RouterLink
-                v-if="
-                  $route.name !== 'statistiques' &&
+                v-if="$route.name !== 'statistiques'&&
                     $route.name !== 'statistiques-globales' &&
-                    $route.name !== 'statistiques-guyane'
-                "
+                    $route.name !== 'statistiques-guyane'"
                 id="cmn-menu-menu-a-statistiques"
                 :to="{ name: 'statistiques' }"
                 class="btn-menu text-decoration-none bold"
@@ -114,7 +135,10 @@
               >
                 Statistiques
               </RouterLink>
-              <div v-else class="bold color-neutral">
+              <div
+                v-else
+                class="bold color-neutral"
+              >
                 Statistiques
               </div>
             </li>
