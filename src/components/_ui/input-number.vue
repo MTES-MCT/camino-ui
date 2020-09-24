317cf9000bab2a0c6e2c588abcd480da97ced6b1
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2020-09-24 11:41:41 +0200

    corrige inputNumber sur firefox vintage

--- src/components/_ui/input-number.vue
+++ src/components/_ui/input-number.vue
@@ -1,6 +1,6 @@
 <template>
   <input
-    :value="value"
+    :value="numberFormatValue"
     type="text"
     :class="{ 'mb-s': value, mb: !value }"
     @change="control($event.target)"
@@ -8,9 +8,16 @@
 </template>
 
 <script>
+import { numberFormat, reversenumberFormat } from '../../utils'
 export default {
   props: {
     value: { type: Number, default: undefined }
+  },
+
+  computed: {
+    numberFormatValue() {
+      return numberFormat(this.value)
+    }
   },
 
   methods: {
@@ -18,7 +25,7 @@ export default {
       const regex = /(^|[^\d.])0+\B/g
       this.$emit(
         'input',
-        parseFloat(target.value.replace(regex, '$1').replaceAll(/ /g, ''))
+        parseFloat(reversenumberFormat(target.value).replace(regex, '$1'))
       )
     }
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,7 +5,20 @@ const dateFormat = dateString => {
 }
 
 const numberFormat = x => {
-  return Intl.NumberFormat('FR-fr').format(x)
+  return Intl.NumberFormat('FR-fr', {
+    minimumSignificantDigits: 1,
+    maximumSignificantDigits: 21
+  }).format(x)
+}
+
+const reversenumberFormat = (val, locale) => {
+  const group = new Intl.NumberFormat(locale).format(1111).replace(/1/g, '')
+  const decimal = new Intl.NumberFormat(locale).format(1.1).replace(/1/g, '')
+  const reversedVal = val
+    .replace(new RegExp('\\' + group, 'g'), '')
+    .replace(new RegExp('\\' + decimal, 'g'), '.')
+    .replace(' ', '')
+  return Number.isNaN(reversedVal) ? 0 : reversedVal
 }
 
 const permissionsCheck = (userPermission, permissions) =>
@@ -15,4 +28,10 @@ const typenameOmit = (key, value) => (key === '__typename' ? undefined : value)
 
 const jsonTypenameOmit = json => JSON.parse(JSON.stringify(json), typenameOmit)
 
-export { dateFormat, permissionsCheck, numberFormat, jsonTypenameOmit }
+export {
+  dateFormat,
+  permissionsCheck,
+  numberFormat,
+  reversenumberFormat,
+  jsonTypenameOmit
+}
