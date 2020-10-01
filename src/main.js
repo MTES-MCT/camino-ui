50129e1dd2bbf9ed80934c65d836760378f3e24f
vmaubert <v.maubert@code-troopers.com>
2020-10-01 14:23:33 +0200

    feat(sentry): permet d’activer ou non Sentry via une variable d’environnement

--- src/main.js
+++ src/main.js
@@ -13,33 +13,45 @@ import mixins from './mixins'
 import { dateFormat } from './utils'
 
 if (process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: 'https://c8050c650ac54b04bbc47175763446c0@sentry.io/1332560',
-    integrations: [
-      new SentryIntegrations.Vue({
-        Vue,
-        attachProps: true
-      }),
-      new SentryIntegrations.RewriteFrames()
-    ],
-    /* global npmVersion */
-    // @ts-ignore
-    release: `camino-ui-${npmVersion}`
-  })
+  fetch('/sentryDsn')
+    .then(response => response.text())
+    .then(dsn => {
+      if (!dsn) throw new Error('Pas de DSN Sentry')
+      Sentry.init({
+        dsn,
+        integrations: [
+          new SentryIntegrations.Vue({
+            Vue,
+            attachProps: true
+          }),
+          new SentryIntegrations.RewriteFrames()
+        ],
+        /* global npmVersion */
+        // @ts-ignore
+        release: `camino-ui-${npmVersion}`
+      })
+    })
+    .catch(e => console.error('Impossible d’initialiser Sentry', e))
+
+  fetch('/matomoOptions')
+    .then(response => response.json())
+    .then(options => {
+      if (!options || !options.host || !options.siteId)
+        throw new Error('"host" et/ou "siteId" manquant')
+      Vue.use(VueMatomo, {
+        host: options.host,
+        siteId: options.siteId,
+        router,
+        store,
+        requireConsent: false,
+        trackInitialView: true,
+        trackerFileName: 'piwik',
+        enableHeartBeatTimer: true,
+        enableLinkTracking: true
+      })
+    })
+    .catch(e => console.error('Impossible d’initialiser Matomo', e))
 }
-
-Vue.use(VueMatomo, {
-  host: '/matomo',
-  siteId: 'matomo-site-id',
-  router,
-  store,
-  requireConsent: false,
-  trackInitialView: true,
-  trackerFileName: 'piwik',
-  enableHeartBeatTimer: true,
-  enableLinkTracking: true
-})
-
 Vue.config.productionTip = false
 
 Vue.filter('dateFormat', dateFormat)
