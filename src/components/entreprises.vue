3d1860750fe40da5ba8f333be2f88c57169d2ee2
Francois Romain <francoisromain@gmail.com>
2021-03-17 15:48:25 +0100

    feat: met à jour les permissions

--- src/components/entreprises.vue
+++ src/components/entreprises.vue
@@ -14,7 +14,7 @@
     @url-load="urlLoad"
   >
     <button
-      v-if="permissionsCheck(['super', 'admin', 'editeur'])"
+      v-if="user.entreprisesCreation"
       slot="addButton"
       class="btn rnd-xs py-s px-m full-x flex mb-s h5"
       @click="addPopupOpen"
