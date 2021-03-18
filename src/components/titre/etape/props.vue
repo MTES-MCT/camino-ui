7218bfe4997d1ff20256f94812b765662eb91fac
vmaubert <v.maubert@code-troopers.com>
2021-03-18 11:43:45 +0100

    style(étape): modifie l’ordre d’affichage des informations d’une étape

--- src/components/titre/etape/props.vue
+++ src/components/titre/etape/props.vue
@@ -1,5 +1,78 @@
 <template>
   <div class="px-m pt-m border-b-s">
+    <div
+      v-if="etape.substances && etape.substances.length"
+      class="tablet-blobs"
+    >
+      <div class="tablet-blob-1-4">
+        <h6>
+          Substance{{ etape.substances.length > 1 ? 's' : '' }}
+          <Tag
+            v-if="etape.incertitudes && etape.incertitudes.substances"
+            :mini="true"
+            color="bg-info"
+            class="ml-xs"
+          >
+            Incertain
+          </Tag>
+        </h6>
+      </div>
+      <div class="tablet-blob-3-4">
+        <TagList :elements="etape.substances.map(s => s.nom)" />
+      </div>
+    </div>
+    <div
+      v-if="etape.titulaires && etape.titulaires.length"
+      class="tablet-blobs"
+    >
+      <div class="tablet-blob-1-4">
+        <h6>
+          Titulaire{{ etape.titulaires.length > 1 ? 's' : '' }}
+          <Tag
+            v-if="etape.incertitudes && etape.incertitudes.titulaires"
+            :mini="true"
+            color="bg-info ml-xs"
+          >
+            Incertain
+          </Tag>
+        </h6>
+      </div>
+      <div class="tablet-blob-3-4">
+        <ul class="list-prefix mb">
+          <li v-for="t in etape.titulaires" :key="t.id">
+            {{ etablissementNameFind(t.etablissements, etape.date) || t.nom }}
+            <Tag v-if="t.operateur" :mini="true" color="bg-info" class="ml-xs">
+              Opérateur
+            </Tag>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div
+      v-if="etape.amodiataires && etape.amodiataires.length"
+      class="tablet-blobs"
+    >
+      <div class="tablet-blob-1-4">
+        <h6>
+          Amodiataire{{ etape.amodiataires.length > 1 ? 's' : '' }}
+          <Tag
+            v-if="etape.incertitudes && etape.incertitudes.amodiataires"
+            :mini="true"
+            color="bg-info"
+            class="ml-xs"
+          >
+            Incertain
+          </Tag>
+        </h6>
+      </div>
+      <div class="tablet-blob-3-4">
+        <ul class="list-prefix">
+          <li v-for="t in etape.amodiataires" :key="t.id">
+            {{ etablissementNameFind(t.etablissements, etape.date) || t.nom }}
+          </li>
+        </ul>
+      </div>
+    </div>
     <div v-if="etape.duree" class="tablet-blobs">
       <div class="tablet-blob-1-4">
         <h6>
@@ -61,24 +134,6 @@
         </p>
       </div>
     </div>
-    <div v-if="etape.points && etape.points.length" class="tablet-blobs">
-      <div class="tablet-blob-1-4">
-        <h6>
-          Périmètre
-          <Tag
-            v-if="etape.incertitudes && etape.incertitudes.points"
-            :mini="true"
-            color="bg-info"
-            class="ml-xs"
-          >
-            Incertain
-          </Tag>
-        </h6>
-      </div>
-      <div class="tablet-blob-3-4">
-        <Points :points="etape.points" />
-      </div>
-    </div>
     <div v-if="etape.surface" class="tablet-blobs">
       <div class="tablet-blob-1-4">
         <h6>
@@ -97,43 +152,12 @@
         <p>{{ numberFormat(etape.surface) }} km² environ</p>
       </div>
     </div>
-
-    <div
-      v-if="etape.titulaires && etape.titulaires.length"
-      class="tablet-blobs"
-    >
+    <div v-if="etape.points && etape.points.length" class="tablet-blobs">
       <div class="tablet-blob-1-4">
         <h6>
-          Titulaire{{ etape.titulaires.length > 1 ? 's' : '' }}
+          Périmètre
           <Tag
-            v-if="etape.incertitudes && etape.incertitudes.titulaires"
-            :mini="true"
-            color="bg-info ml-xs"
-          >
-            Incertain
-          </Tag>
-        </h6>
-      </div>
-      <div class="tablet-blob-3-4">
-        <ul class="list-prefix mb">
-          <li v-for="t in etape.titulaires" :key="t.id">
-            {{ etablissementNameFind(t.etablissements, etape.date) || t.nom }}
-            <Tag v-if="t.operateur" :mini="true" color="bg-info" class="ml-xs">
-              Opérateur
-            </Tag>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div
-      v-if="etape.amodiataires && etape.amodiataires.length"
-      class="tablet-blobs"
-    >
-      <div class="tablet-blob-1-4">
-        <h6>
-          Amodiataire{{ etape.amodiataires.length > 1 ? 's' : '' }}
-          <Tag
-            v-if="etape.incertitudes && etape.incertitudes.amodiataires"
+            v-if="etape.incertitudes && etape.incertitudes.points"
             :mini="true"
             color="bg-info"
             class="ml-xs"
@@ -143,32 +167,7 @@
         </h6>
       </div>
       <div class="tablet-blob-3-4">
-        <ul class="list-prefix">
-          <li v-for="t in etape.amodiataires" :key="t.id">
-            {{ etablissementNameFind(t.etablissements, etape.date) || t.nom }}
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div
-      v-if="etape.substances && etape.substances.length"
-      class="tablet-blobs"
-    >
-      <div class="tablet-blob-1-4">
-        <h6>
-          Substance{{ etape.substances.length > 1 ? 's' : '' }}
-          <Tag
-            v-if="etape.incertitudes && etape.incertitudes.substances"
-            :mini="true"
-            color="bg-info"
-            class="ml-xs"
-          >
-            Incertain
-          </Tag>
-        </h6>
-      </div>
-      <div class="tablet-blob-3-4">
-        <TagList :elements="etape.substances.map(s => s.nom)" />
+        <Points :points="etape.points" />
       </div>
     </div>
   </div>
