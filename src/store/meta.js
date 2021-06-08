45a15ec6cdf145f3dd09b67117e5d98930d305f6
nicolas.petitot <nicolas.petitot@developpement-durable.gouv.fr>
2021-06-08 11:03:42 +0200

    refacto: syntaxe en dot

--- src/store/meta.js
+++ src/store/meta.js
@@ -15,53 +15,51 @@ const actions = {
         const res = await definition.get()
         let elements = null
         if (res) {
-          if (id === 'definitions') elements = res['definitions']
-          if (id === 'domaines') elements = res['domaines']
-          if (id === 'titres-types-types') elements = res['types']
-          if (id === 'titres-types') elements = res['titresTypes']
-          if (id === 'titres-statuts') elements = res['statuts']
+          if (id === 'definitions') elements = res.definitions
+          if (id === 'domaines') elements = res.domaines
+          if (id === 'titres-types-types') elements = res.types
+          if (id === 'titres-types') elements = res.titresTypes
+          if (id === 'titres-statuts') elements = res.statuts
           if (id === 'titre-types--titres-statuts')
-            elements = res['titresTypesTitresStatuts']
-          if (id === 'demarches-types') elements = res['demarchesTypes']
+            elements = res.titresTypesTitresStatuts
+          if (id === 'demarches-types') elements = res.demarchesTypes
           if (id === 'titres-types--demarches-types')
-            elements = res['titresTypesDemarchesTypes']
-          if (id === 'demarches-statuts') elements = res['demarchesStatuts']
-          if (id === 'phases-statuts') elements = res['phasesStatuts']
-          if (id === 'etapes-types') elements = res['etapesTypes']
+            elements = res.titresTypesDemarchesTypes
+          if (id === 'demarches-statuts') elements = res.demarchesStatuts
+          if (id === 'phases-statuts') elements = res.phasesStatuts
+          if (id === 'etapes-types') elements = res.etapesTypes
           if (id === 'titres-types--demarches-types--etapes-types')
-            elements = res['titresTypesDemarchesTypesEtapesTypes']
-          if (id === 'etapes-statuts') elements = res['etapesStatuts']
+            elements = res.titresTypesDemarchesTypesEtapesTypes
+          if (id === 'etapes-statuts') elements = res.etapesStatuts
           if (id === 'etapes-types--etapes-statuts')
-            elements = res['etapesTypesEtapesStatuts']
+            elements = res.etapesTypesEtapesStatuts
           if (id === 'etapes-types--documents-types')
-            elements = res['etapesTypesDocumentsTypes']
+            elements = res.etapesTypesDocumentsTypes
           if (id === 'etapes-types--justificatifs-types')
-            elements = res['etapesTypesJustificatifsTypes']
-          if (id === 'travaux-types') elements = res['travauxTypes']
-          if (id === 'travaux-etapes-types')
-            elements = res['travauxEtapesTypes']
+            elements = res.etapesTypesJustificatifsTypes
+          if (id === 'travaux-types') elements = res.travauxTypes
+          if (id === 'travaux-etapes-types') elements = res.travauxEtapesTypes
           if (id === 'travaux-types--travaux-etapes-types')
-            elements = res['travauxTypesTravauxEtapesTypes']
+            elements = res.travauxTypesTravauxEtapesTypes
           if (id === 'travaux-etapes-types--documents-types')
-            elements = res['travauxEtapesTypesDocumentsTypes']
+            elements = res.travauxEtapesTypesDocumentsTypes
           if (id === 'travaux-etapes-types--etapes-statuts')
-            elements = res['travauxEtapesTypesEtapesStatuts']
-          if (id === 'devises') elements = res['devises']
-          if (id === 'unites') elements = res['unites']
+            elements = res.travauxEtapesTypesEtapesStatuts
+          if (id === 'devises') elements = res.devises
+          if (id === 'unites') elements = res.unites
           if (id === 'administrations-types')
-            elements = res['administrationsTypes']
-          if (id === 'permissions') elements = res['permissions']
-          if (id === 'documents-types') elements = res['documentsTypes']
-          if (id === 'references-types') elements = res['referencesTypes']
-          if (id === 'geo-systemes') elements = res['geoSystemes']
-          if (id === 'activites-types') elements = res['activitesTypes']
-          if (id === 'activites-statuts') elements = res['activitesStatuts']
+            elements = res.administrationsTypes
+          if (id === 'permissions') elements = res.permissions
+          if (id === 'documents-types') elements = res.documentsTypes
+          if (id === 'references-types') elements = res.referencesTypes
+          if (id === 'geo-systemes') elements = res.geoSystemes
+          if (id === 'activites-types') elements = res.activitesTypes
+          if (id === 'activites-statuts') elements = res.activitesStatuts
           if (id === 'activites-types--titres-types')
-            elements = res['activitesTypesTitresTypes']
+            elements = res.activitesTypesTitresTypes
           if (id === 'activites-types--documents-types')
-            elements = res['activitesTypesDocumentsTypes']
-          if (id === 'activites-types--pays')
-            elements = res['activitesTypesPays']
+            elements = res.activitesTypesDocumentsTypes
+          if (id === 'activites-types--pays') elements = res.activitesTypesPays
         }
         commit('set', {
           elements,
@@ -84,52 +82,49 @@ const actions = {
         const res = await definition.update({ element })
         let elements = null
         if (res) {
-          if (id === 'definitions') elements = res['definitionModifier']
-          if (id === 'domaines') elements = res['domaineModifier']
-          if (id === 'titres-types-types')
-            elements = res['titreTypeTypeModifier']
-          if (id === 'titres-types') elements = res['titreTypeModifier']
-          if (id === 'titres-statuts') elements = res['titreStatutModifier']
+          if (id === 'definitions') elements = res.definitionModifier
+          if (id === 'domaines') elements = res.domaineModifier
+          if (id === 'titres-types-types') elements = res.titreTypeTypeModifier
+          if (id === 'titres-types') elements = res.titreTypeModifier
+          if (id === 'titres-statuts') elements = res.titreStatutModifier
           if (id === 'titre-types--titres-statuts')
-            elements = res['titreTypeTitreStatutModifier']
-          if (id === 'demarches-types') elements = res['demarcheTypeModifier']
+            elements = res.titreTypeTitreStatutModifier
+          if (id === 'demarches-types') elements = res.demarcheTypeModifier
           if (id === 'titres-types--demarches-types')
-            elements = res['titreTypeDemarcheTypeModifier']
-          if (id === 'demarches-statuts')
-            elements = res['demarcheStatutModifier']
-          if (id === 'phases-statuts') elements = res['phaseStatutModifier']
-          if (id === 'etapes-types') elements = res['etapeTypeModifier']
+            elements = res.titreTypeDemarcheTypeModifier
+          if (id === 'demarches-statuts') elements = res.demarcheStatutModifier
+          if (id === 'phases-statuts') elements = res.phaseStatutModifier
+          if (id === 'etapes-types') elements = res.etapeTypeModifier
           if (id === 'titres-types--demarches-types--etapes-types')
-            elements = res['titreTypeDemarcheTypeEtapeTypeModifier']
-          if (id === 'etapes-statuts') elements = res['etapeStatutModifier']
+            elements = res.titreTypeDemarcheTypeEtapeTypeModifier
+          if (id === 'etapes-statuts') elements = res.etapeStatutModifier
           if (id === 'etapes-types--etapes-statuts')
-            elements = res['etapeTypeEtapeStatutModifier']
+            elements = res.etapeTypeEtapeStatutModifier
           if (id === 'etapes-types--documents-types')
-            elements = res['etapeTypeDocumentTypeModifier']
+            elements = res.etapeTypeDocumentTypeModifier
           if (id === 'etapes-types--justificatifs-types')
-            elements = res['etapeTypeJustificatifTypeModifier']
-          if (id === 'travaux-types') elements = res['travauxTypeModifier']
+            elements = res.etapeTypeJustificatifTypeModifier
+          if (id === 'travaux-types') elements = res.travauxTypeModifier
           if (id === 'travaux-etapes-types')
-            elements = res['travauxEtapeTypeModifier']
+            elements = res.travauxEtapeTypeModifier
           if (id === 'travaux-types--travaux-etapes-types')
-            elements = res['travauxTypeTravauxEtapeTypeModifier']
+            elements = res.travauxTypeTravauxEtapeTypeModifier
           if (id === 'travaux-etapes-types--documents-types')
-            elements = res['travauxEtapeTypeDocumentTypeModifier']
+            elements = res.travauxEtapeTypeDocumentTypeModifier
           if (id === 'travaux-etapes-types--etapes-statuts')
-            elements = res['travauxEtapeTypeEtapeStatutModifier']
-          if (id === 'devises') elements = res['deviseModifier']
-          if (id === 'unites') elements = res['uniteModifier']
+            elements = res.travauxEtapeTypeEtapeStatutModifier
+          if (id === 'devises') elements = res.deviseModifier
+          if (id === 'unites') elements = res.uniteModifier
           if (id === 'administrations-types')
-            elements = res['administrationTypeModifier']
-          if (id === 'permissions') elements = res['permissionModifier']
-          if (id === 'documents-types') elements = res['documentTypeModifier']
-          if (id === 'references-types') elements = res['referenceTypeModifier']
-          if (id === 'geo-systemes') elements = res['geoSystemeModifier']
-          if (id === 'activites-types') elements = res['activiteTypeModifier']
-          if (id === 'activites-statuts')
-            elements = res['activiteStatutModifier']
+            elements = res.administrationTypeModifier
+          if (id === 'permissions') elements = res.permissionModifier
+          if (id === 'documents-types') elements = res.documentTypeModifier
+          if (id === 'references-types') elements = res.referenceTypeModifier
+          if (id === 'geo-systemes') elements = res.geoSystemeModifier
+          if (id === 'activites-types') elements = res.activiteTypeModifier
+          if (id === 'activites-statuts') elements = res.activiteStatutModifier
           if (id === 'activites-types--documents-types')
-            elements = res['activiteTypeDocumentTypeModifier']
+            elements = res.activiteTypeDocumentTypeModifier
         }
 
         commit('set', {
@@ -153,31 +148,31 @@ const actions = {
         const res = await definition.create({ element })
         let elements = null
         if (res) {
-          if (id === 'titres-types') elements = res['titreTypeCreer']
+          if (id === 'titres-types') elements = res.titreTypeCreer
           if (id === 'titre-types--titres-statuts')
-            elements = res['titreTypeTitreStatutCreer']
+            elements = res.titreTypeTitreStatutCreer
           if (id === 'titres-types--demarches-types')
-            elements = res['titreTypeDemarcheTypeCreer']
+            elements = res.titreTypeDemarcheTypeCreer
           if (id === 'titres-types--demarches-types--etapes-types')
-            elements = res['titreTypeDemarcheTypeEtapeTypeCreer']
+            elements = res.titreTypeDemarcheTypeEtapeTypeCreer
           if (id === 'etapes-types--etapes-statuts')
-            elements = res['etapeTypeEtapeStatutCreer']
+            elements = res.etapeTypeEtapeStatutCreer
           if (id === 'etapes-types--documents-types')
-            elements = res['etapeTypeDocumentTypeCreer']
+            elements = res.etapeTypeDocumentTypeCreer
           if (id === 'etapes-types--justificatifs-types')
-            elements = res['etapeTypeJustificatifTypeCreer']
+            elements = res.etapeTypeJustificatifTypeCreer
           if (id === 'travaux-types--travaux-etapes-types')
-            elements = res['travauxTypeTravauxEtapeTypeCreer']
+            elements = res.travauxTypeTravauxEtapeTypeCreer
           if (id === 'travaux-etapes-types--documents-types')
-            elements = res['travauxEtapeTypeDocumentTypeCreer']
+            elements = res.travauxEtapeTypeDocumentTypeCreer
           if (id === 'travaux-etapes-types--etapes-statuts')
-            elements = res['travauxEtapeTypeEtapeStatutCreer']
+            elements = res.travauxEtapeTypeEtapeStatutCreer
           if (id === 'activites-types--titres-types')
-            elements = res['activiteTypeTitreTypeCreer']
+            elements = res.activiteTypeTitreTypeCreer
           if (id === 'activites-types--documents-types')
-            elements = res['activiteTypeDocumentTypeCreer']
+            elements = res.activiteTypeDocumentTypeCreer
           if (id === 'activites-types--pays')
-            elements = res['activiteTypePaysCreer']
+            elements = res.activiteTypePaysCreer
         }
 
         commit('set', {
@@ -201,31 +196,31 @@ const actions = {
         const res = await definition.delete({ element })
         let elements = null
         if (res) {
-          if (id === 'titres-types') elements = res['titreTypeSupprimer']
+          if (id === 'titres-types') elements = res.titreTypeSupprimer
           if (id === 'titre-types--titres-statuts')
-            elements = res['titreTypeTitreStatutSupprimer']
+            elements = res.titreTypeTitreStatutSupprimer
           if (id === 'titres-types--demarches-types')
-            elements = res['titreTypeDemarcheTypeSupprimer']
+            elements = res.titreTypeDemarcheTypeSupprimer
           if (id === 'titres-types--demarches-types--etapes-types')
-            elements = res['titreTypeDemarcheTypeEtapeTypeSupprimer']
+            elements = res.titreTypeDemarcheTypeEtapeTypeSupprimer
           if (id === 'etapes-types--etapes-statuts')
-            elements = res['etapeTypeEtapeStatutSupprimer']
+            elements = res.etapeTypeEtapeStatutSupprimer
           if (id === 'etapes-types--documents-types')
-            elements = res['etapeTypeDocumentTypeSupprimer']
+            elements = res.etapeTypeDocumentTypeSupprimer
           if (id === 'etapes-types--justificatifs-types')
-            elements = res['etapeTypeJustificatifTypeSupprimer']
+            elements = res.etapeTypeJustificatifTypeSupprimer
           if (id === 'travaux-types--travaux-etapes-types')
-            elements = res['travauxTypeTravauxEtapeTypeSupprimer']
+            elements = res.travauxTypeTravauxEtapeTypeSupprimer
           if (id === 'travaux-etapes-types--documents-types')
-            elements = res['travauxEtapeTypeDocumentTypeSupprimer']
+            elements = res.travauxEtapeTypeDocumentTypeSupprimer
           if (id === 'travaux-etapes-types--etapes-statuts')
-            elements = res['travauxEtapeTypeEtapeStatutSupprimer']
+            elements = res.travauxEtapeTypeEtapeStatutSupprimer
           if (id === 'activites-types--titres-types')
-            elements = res['activiteTypeTitreTypeSupprimer']
+            elements = res.activiteTypeTitreTypeSupprimer
           if (id === 'activites-types--documents-types')
-            elements = res['activiteTypeDocumentTypeSupprimer']
+            elements = res.activiteTypeDocumentTypeSupprimer
           if (id === 'activites-types--pays')
-            elements = res['activiteTypePaysSupprimer']
+            elements = res.activiteTypePaysSupprimer
         }
 
         commit('set', {
