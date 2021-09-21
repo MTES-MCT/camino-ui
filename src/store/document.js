8fc38fcebaae87ca98901b0a1d2fefd9fea9c585
Davy Peter BRAUN <hello@davybraun.com>
2021-09-21 18:37:35 +0200

    refact(upload): rebranche la modification de documents

--- src/store/document.js
+++ src/store/document.js
@@ -1,7 +1,7 @@
 import {
   documentMetas,
   documentCreer,
-  // documentModifier,
+  documentModifier,
   documentSupprimer
 } from '../api/documents'
 import { uploadCall } from '../api/_client'
@@ -40,54 +40,53 @@ const actions = {
         delete document.id
       }
 
+      let d
+
       if (!document.id) {
-        const { fichierNouveau } = document
-        if (!fichierNouveau) return
-
-        // Pour ne pas uploader de fichier via GraphQL
-        delete document.fichierNouveau
-
-        const updatedDocument = await documentCreer({ document })
-
-        await uploadCall(fichierNouveau, updatedDocument.id, progress => {
-          commit('fileLoad', { loaded: progress, total: 100 }, { root: true })
-        })
-
-        setTimeout(() => {
-          commit('fileLoad', { loaded: 0, total: 0 }, { root: true })
-          commit('popupClose', null, { root: true })
-        }, 500)
-
-        dispatch(
-          'messageAdd',
-          { value: `le document a été mis à jour`, type: 'success' },
-          { root: true }
-        )
-
-        if (route) {
-          await dispatch('reload', route, { root: true })
-
-          if (route.name === 'titre') {
-            const section = route.section
-            let id
-
-            if (section === 'etapes') id = document.titreEtapeId
-            if (section === 'travaux') id = document.titreTravauxEtapeId
-
-            commit('titre/open', { section, id }, { root: true })
-          }
-        } else if (action) {
-          const params = { ...action.params, document: updatedDocument }
-
-          if (idOld) {
-            params.idOld = idOld
-          }
-
-          await dispatch(action.name, params, { root: true })
-        }
+        d = await documentCreer({ document })
       } else {
         delete document.typeId
-        // d = await documentModifier({ document })
+        d = await documentModifier({ document })
+      }
+
+      const { fichierNouveau } = document
+      if (!fichierNouveau) return
+
+      await uploadCall(fichierNouveau, d.id, progress => {
+        commit('fileLoad', { loaded: progress, total: 100 }, { root: true })
+      })
+
+      setTimeout(() => {
+        commit('fileLoad', { loaded: 0, total: 0 }, { root: true })
+        commit('popupClose', null, { root: true })
+      }, 500)
+
+      dispatch(
+        'messageAdd',
+        { value: `le document a été mis à jour`, type: 'success' },
+        { root: true }
+      )
+
+      if (route) {
+        await dispatch('reload', route, { root: true })
+
+        if (route.name === 'titre') {
+          const section = route.section
+          let id
+
+          if (section === 'etapes') id = document.titreEtapeId
+          if (section === 'travaux') id = document.titreTravauxEtapeId
+
+          commit('titre/open', { section, id }, { root: true })
+        }
+      } else if (action) {
+        const params = { ...action.params, document: d }
+
+        if (idOld) {
+          params.idOld = idOld
+        }
+
+        await dispatch(action.name, params, { root: true })
       }
     } catch (e) {
       commit('popupMessageAdd', { value: e, type: 'error' }, { root: true })
