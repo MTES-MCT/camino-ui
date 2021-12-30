b57c4222a0c676057ab4750f8d936ebc0ac643c6
vmaubert <v.maubert@code-troopers.com>
2021-12-30 16:04:31 +0100

    feat(étape): affiche des messages lors de l’édition d’une étape (#452)

--- src/components/etape/form-save-btn.vue
+++ src/components/etape/form-save-btn.vue
@@ -1,26 +1,38 @@
 <template>
   <div v-bind="$attrs">
-    <HelpTooltip v-if="armHelpVisible" class="mr-m">
-      Vous pouvez à tout moment enregistrer votre demande. Le dépôt du dossier
-      d’ARM et de toutes les pièces peut être réalisé en plusieurs fois. Vous
-      pourrez compléter votre demande en cliquant sur
-      <span class="inline-block"><i class="icon-24 icon-pencil" /></span>. Si
-      vous avez ajouté tous les documents spécifiques à la demande d’ARM et
-      justificatifs d’entreprise, et que vous considérez que votre demande est
-      complète, vous pouvez la déposer à l’étape suivante en cliquant sur «
-      Déposer … ». L’ONF et le PTMG seront ainsi notifiés et pourront instruire
-      votre demande.
-    </HelpTooltip>
-    <button
-      id="cmn-etape-edit-button-enregistrer"
-      ref="save-btn"
-      class="btn btn-primary"
-      :disabled="$attrs.disabled"
-      :class="{ disabled: $attrs.disabled }"
-      @click="$emit('click')"
-    >
-      Enregistrer
-    </button>
+    <div class="flex flex-direction-column full-x">
+      <ul
+        v-if="messages?.length"
+        class="bg-warning color-bg list-sans p-s bold"
+      >
+        <li v-for="message in messages" :key="message">
+          {{ message }}
+        </li>
+      </ul>
+      <div>
+        <HelpTooltip v-if="armHelpVisible" class="mr-m">
+          Vous pouvez à tout moment enregistrer votre demande. Le dépôt du
+          dossier d’ARM et de toutes les pièces peut être réalisé en plusieurs
+          fois. Vous pourrez compléter votre demande en cliquant sur
+          <span class="inline-block"><i class="icon-24 icon-pencil" /></span>.
+          Si vous avez ajouté tous les documents spécifiques à la demande d’ARM
+          et justificatifs d’entreprise, et que vous considérez que votre
+          demande est complète, vous pouvez la déposer à l’étape suivante en
+          cliquant sur « Déposer … ». L’ONF et le PTMG seront ainsi notifiés et
+          pourront instruire votre demande.
+        </HelpTooltip>
+        <button
+          id="cmn-etape-edit-button-enregistrer"
+          ref="save-btn"
+          class="btn btn-primary"
+          :disabled="$attrs.disabled"
+          :class="{ disabled: $attrs.disabled }"
+          @click="$emit('click')"
+        >
+          Enregistrer
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,7 +47,8 @@ export default defineComponent({
     armHelpVisible: {
       type: Boolean,
       default: false
-    }
+    },
+    messages: { type: Array, default: () => [] }
   },
 
   emits: ['click'],
