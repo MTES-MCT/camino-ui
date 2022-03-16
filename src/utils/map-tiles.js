52575ee835702e31bd4c266b34b26680fb610de7
vmaubert <v.maubert@code-troopers.com>
2022-03-16 10:44:24 +0100

    fix(carte): corrige les fonds de cartes IGN et supprime les fonds de cartes obsolètes (#493)

--- src/utils/map-tiles.js
+++ src/utils/map-tiles.js
@@ -1,13 +1,5 @@
 export default [
   {
-    id: 'osm-mapnik',
-    name: 'OSM / mapnik',
-    type: 'tiles',
-    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  },
-  {
     id: 'osm-fr',
     name: 'OSM / fr',
     type: 'tiles',
@@ -25,37 +17,23 @@ export default [
   },
   {
     id: 'geoportail-ign-map',
-    name: 'Géoportail / Carte IGN',
-    type: 'tiles',
-    url: 'https://wxs.ign.fr/ff8nyjqym1ym7bz3mw6mpehc/geoportail/wmts?&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/jpeg&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
-    attribution: 'IGN-F/Geoportail'
-  },
-  {
-    id: 'geoportail-scan-express-standard',
-    name: 'Géoportail / Scan Express Standard',
-    type: 'tiles',
-    url: 'https://wxs.ign.fr/ff8nyjqym1ym7bz3mw6mpehc/geoportail/wmts?&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/jpeg&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
-    attribution: 'IGN-F/Geoportail'
-  },
-  {
-    id: 'geoportail-ign-plan',
     name: 'Géoportail / Plan IGN',
     type: 'tiles',
-    url: 'https://wxs.ign.fr/ff8nyjqym1ym7bz3mw6mpehc/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix={z}&TileCol={x}&TileRow={y}',
+    url: 'https://wxs.ign.fr/essentiels/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix={z}&TileCol={x}&TileRow={y}',
     attribution: 'IGN-F/Geoportail'
   },
   {
     id: 'geoportail-photographies-aeriennes',
     name: 'Géoportail / Photographies aériennes',
     type: 'tiles',
-    url: 'https://wxs.ign.fr/ff8nyjqym1ym7bz3mw6mpehc/geoportail/wmts?&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/jpeg&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    url: 'https://wxs.ign.fr/essentiels/geoportail/wmts?&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/jpeg&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
     attribution: 'IGN-F/Geoportail'
   },
   {
     id: 'geoportail-parcelles-cadastrales',
     name: 'Géoportail / Parcelles cadastrales',
     type: 'tiles',
-    url: 'https://wxs.ign.fr/ff8nyjqym1ym7bz3mw6mpehc/geoportail/wmts?&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/png&LAYER=CADASTRALPARCELS.PARCELS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+    url: 'https://wxs.ign.fr/parcellaire/geoportail/wmts?&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE=normal&TILEMATRIXSET=PM&FORMAT=image/png&LAYER=CADASTRALPARCELS.PARCELS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
     attribution: 'IGN-F/Geoportail'
   },
   {
@@ -64,7 +42,8 @@ export default [
     type: 'wms',
     url: 'https://geoservices.brgm.fr/geologie',
     layers: 'SCAN_H_GEOL50',
-    attribution: 'BRGM'
+    attribution: 'BRGM',
+    version: '1.3.0'
   },
   {
     id: 'sdom-zones',
